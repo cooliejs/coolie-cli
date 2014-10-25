059135e5c41eee2e768adcb2bed6d38c543bb572
cloudcome <cloudcome@163.com>
2014-10-25 19:12:56 +0800

    精简

--- libs/jsminify.js
+++ libs/jsminify.js
@@ -9,53 +9,52 @@
 var uglifyJS = require("uglify-js");
 var log = require('./log.js');
 var util = require('./util.js');
-var yuicompressor = require('yuicompressor');
-var compressorOptions = {
-    // 连续单语句，逗号分开
-    // 如： alert(1);alert(2); => alert(1),alert(2)
-    sequences: false,
-    // 重写属性
-    // 如：foo['bar'] => foo.bar
-    properties: false,
-    // 删除无意义代码
-    dead_code: false,
-    // 移除`debugger;`
-    drop_debugger: true,
-    // 使用以下不安全的压缩
-    unsafe: false,
-    //
-    unsafe_comps: false,
-    // 压缩if表达式
-    conditionals: false,
-    // 压缩条件表达式
-    comparisons: false,
-    // 压缩常数表达式
-    evaluate: false,
-    // 压缩布尔值
-    booleans: false,
-    // 压缩循环
-    loops: false,
-    // 移除未使用变量
-    unused: false,
-    // 函数声明提前
-    hoist_funs: false,
-    // 变量声明提前
-    hoist_vars: false,
-    // 压缩 if return if continue
-    if_return: false,
-    // 合并连续变量省略
-    join_vars: true,
-    // 小范围连续变量压缩
-    cascade: false,
-    // 不显示警告语句
-    warnings: false,
-    side_effects: false,
-    pure_getters: false,
-    pure_funcs: false,
-    negate_iife: false,
-    // 全局变量
-    global_defs: {}
-};
+//var compressorOptions = {
+//    // 连续单语句，逗号分开
+//    // 如： alert(1);alert(2); => alert(1),alert(2)
+//    sequences: false,
+//    // 重写属性
+//    // 如：foo['bar'] => foo.bar
+//    properties: false,
+//    // 删除无意义代码
+//    dead_code: false,
+//    // 移除`debugger;`
+//    drop_debugger: true,
+//    // 使用以下不安全的压缩
+//    unsafe: false,
+//    //
+//    unsafe_comps: false,
+//    // 压缩if表达式
+//    conditionals: false,
+//    // 压缩条件表达式
+//    comparisons: false,
+//    // 压缩常数表达式
+//    evaluate: false,
+//    // 压缩布尔值
+//    booleans: false,
+//    // 压缩循环
+//    loops: false,
+//    // 移除未使用变量
+//    unused: false,
+//    // 函数声明提前
+//    hoist_funs: false,
+//    // 变量声明提前
+//    hoist_vars: false,
+//    // 压缩 if return if continue
+//    if_return: false,
+//    // 合并连续变量省略
+//    join_vars: true,
+//    // 小范围连续变量压缩
+//    cascade: false,
+//    // 不显示警告语句
+//    warnings: false,
+//    side_effects: false,
+//    pure_getters: false,
+//    pure_funcs: false,
+//    negate_iife: false,
+//    // 全局变量
+//    global_defs: {}
+//};
 
 
 /**
