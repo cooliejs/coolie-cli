67510119d9bf1ea0d3437fbe89bd3acbb645b0d8
cloudcome <cloudcome@163.com>
2014-11-20 15:08:05 +0800

    支持生成资源引用关系MAP

--- build/build-modules.js
+++ build/build-modules.js
@@ -30,28 +30,12 @@ module.exports = function (buildPath) {
     var htmlLength = 0;
     var cssLength = 0;
     var versionMap = {};
+    var JSRelationshipMap = {};
+    var HTMLCSSRelationshipMap = {};
 
     howdo
-        // 这里采用增量覆盖，而不是全部清空
-        //.task(function (next) {
-        //    log('1/5', 'clean dest', 'task');
-        //    next();
-        //})
-        //.task(function (next) {
-        //    fs.remove(destPath, function (err) {
-        //        if (err) {
-        //            log('remove', ydrUtil.dato.fixPath(destPath), 'error');
-        //            log('remove', err.message, 'error');
-        //            process.exit();
-        //        }
-        //
-        //        log('remove', ydrUtil.dato.fixPath(destPath), 'success');
-        //        next();
-        //    });
-        //})
-
         .task(function (next) {
-            log('1/4', 'copy files', 'task');
+            log('1/5', 'copy files', 'task');
             next();
         })
         .each(config.copy, function (i, copyFile, nextCopy) {
@@ -90,7 +74,7 @@ module.exports = function (buildPath) {
         })
 
         .task(function (next) {
-            log('2/4', 'build main', 'task');
+            log('2/5', 'build main', 'task');
             next();
         })
         .each(config.js, function (i, main, nextMain) {
@@ -109,10 +93,16 @@ module.exports = function (buildPath) {
                 howdo.each(files, function (j, file, nextFile) {
                     var relative = path.relative(srcPath, file);
 
-                    buildMain(file, function (err, code, md5List) {
+                    buildMain(file, function (err, code, md5List, deepDeps) {
                         if (err) {
                             return;
                         }
+
+                        var depNames = deepDeps.map(function(dep){
+                            return ydrUtil.dato.toURLPath(path.relative(srcPath, dep));
+                        });
+
+                        JSRelationshipMap[ydrUtil.dato.toURLPath(relative)] = depNames;
 
                         var md5Version = ydrUtil.crypto.md5(md5List).slice(0, 8);
                         var destFile = path.join(destPath, relative);
@@ -139,7 +129,7 @@ module.exports = function (buildPath) {
         })
 
         .task(function (next) {
-            log('3/4', 'overwrite config', 'task');
+            log('3/5', 'overwrite config', 'task');
             next();
         })
         .task(function (next) {
@@ -162,38 +152,64 @@ module.exports = function (buildPath) {
         })
 
         .task(function (next) {
-            log('4/4', 'build html css', 'task');
+            log('4/5', 'build html css', 'task');
             next();
         })
-        .each(config.html, function (i, htmlFile, nextHTML) {
+        .each(config.html, function (i, htmlFile, nextGlob) {
             // html files
             var gbPath = path.join(buildPath, htmlFile);
             var cssPath = path.join(buildPath, config.css);
 
             log('html files', ydrUtil.dato.fixPath(gbPath));
 
-            glob(gbPath, function (err, files) {
+            glob(gbPath, function (err, htmls) {
                 if (err) {
                     log('glob', ydrUtil.dato.fixPath(gbPath), 'error');
                     log('glob', err.message, 'error');
                     process.exit();
                 }
-                howdo.each(files, function (j, file, nextFile) {
+                howdo.each(htmls, function (j, file, nextHTML) {
                     htmlLength++;
-                    buildHTML(file, cssPath, srcPath, destPath, function (err, _cssLength) {
+
+                    buildHTML(file, cssPath, srcPath, destPath, function (err, _cssLength, depCSS) {
+                        var htmlRelative = path.relative(srcPath, file);
+                        var url = ydrUtil.dato.toURLPath(htmlRelative);
+
+                        HTMLCSSRelationshipMap[url] = depCSS;
                         cssLength += _cssLength;
-                        nextFile(err);
+                        nextHTML(err);
                     });
                 }).follow(function () {
-                    nextHTML();
+                    nextGlob();
                 });
             });
         })
 
-        // 结束了
+        .task(function (next) {
+            log('5/5', 'generator relationship map', 'task');
+            next();
+        })
+        .task(function (next) {
+            var map = ydrUtil.dato.extend(JSRelationshipMap, HTMLCSSRelationshipMap);
+            var mapFile = path.join(destPath, './relationship-map.json');
+
+            fs.outputJSON(mapFile, map, function (err) {
+                if (err) {
+                    log('write relationship map', ydrUtil.dato.fixPath(mapFile), 'error');
+                    log('write relationship map', err.message, 'error');
+                    return process.exit();
+                }
+
+                log('write relationship map', ydrUtil.dato.fixPath(mapFile), 'success');
+                next();
+            });
+        })
+
+        // 异步串行结束
         .follow(function (err) {
             if (err) {
-                return;
+                log('unknow error', err.message, 'error');
+                return process.exit();
             }
 
             var past = Date.now() - time;
