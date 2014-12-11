2b4ec53b12cea29e89c4ab5fff6d1c676c40202e
cloudcome <cloudcome@163.com>
2014-12-11 13:59:32 +0800

    coolie

--- example/dest/static/js/coolie-config.js
+++ example/dest/static/js/coolie-config.js
@@ -1,1 +1,8 @@
-coolie.config({base:"./app/",version:{"static/js/app/index.js":"d5964d13","static/js/app/jquery.js":"74be1697","static/js/app/user/index.js":"020eb29b"}}).use();
+coolie.config({
+    base: "./app/",
+    version: {
+        "static/js/app/index.js": "d5964d13",
+        "static/js/app/jquery.js": "74be1697",
+        "static/js/app/user/index.js": "020eb29b"
+    }
+}).use();
