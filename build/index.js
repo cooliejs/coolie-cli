87b29ea2341f075190676efb68878f53ffb0ef54
cloudcome <cloudcome@163.com>
2014-12-11 18:34:57 +0800

    pull coolie.min.js

--- build/index.js
+++ build/index.js
@@ -47,8 +47,8 @@ switch ((cmdArg0 || "").toLowerCase()) {
 
     default:
         log(true, "coolie wiki", "打开 WIKI 页面", "success");
-        log(true, "coolie pull [path]", "下载 coolie.min.js 到指定目录", "success");
         log(true, "coolie version", "输出版本号", "success");
+        log(true, "coolie pull [path]", "下载 coolie.min.js 到指定目录", "success");
         log(true, "coolie config [path]", "在指定目录生成`coolie-config.js`", "success");
         log(true, "coolie json [path]", "在指定目录生成`coolie.json`", "success");
         log(true, "coolie build [path]", "在指定目录根据`coolie.json`配置文件执行构建HTML/JS/CSS", "success");
