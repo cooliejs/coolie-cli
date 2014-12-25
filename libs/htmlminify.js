e11a067306f46f0a262b1eafe17fab0018aea948
cloudcome <cloudcome@163.com>
2014-12-25 17:11:51 +0800

    config.version

--- libs/htmlminify.js
+++ libs/htmlminify.js
@@ -10,6 +10,7 @@ var log = require('./log.js');
 var dato = require('ydr-util').dato;
 var REG_LINES = /[\n\r\t]/g;
 var REG_SPACES = /\s{2,}/g;
+var REG_COMMENTS = /<!--[\s\S]*?-->/g;
 
 
 /**
@@ -20,6 +21,7 @@ var REG_SPACES = /\s{2,}/g;
 module.exports = function (file, code, callback) {
     code = code
         .replace(REG_LINES, '')
+        .replace(REG_COMMENTS, '')
         .replace(REG_SPACES, ' ');
 
     if (callback) {
