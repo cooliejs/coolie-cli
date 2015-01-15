6263e388b0210d962cb109804205c39b5c315092
云淡然 <cloudcome@163.com>
2015-01-15 18:28:24 +0800

    build res

--- example/dest/static/js/app/user/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/user/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie 1421317421930*/
+/*coolie 1421317674853*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(){"use strict";console.log("libs/all.js")});
