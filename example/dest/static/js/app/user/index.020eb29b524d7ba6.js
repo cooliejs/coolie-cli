4540a7da8f5da883ae20655426cef9171eb4ceab
云淡然 <cloudcome@163.com>
2015-02-11 13:39:13 +0800

    html 文件的压缩策略

--- example/dest/static/js/app/user/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/user/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie 1423566181158*/
+/*coolie 1423633102577*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(){"use strict";console.log("libs/all.js")});
--- libs/htmlminify.js
+++ libs/htmlminify.js
@@ -8,25 +8,57 @@
 
 var log = require('./log.js');
 var dato = require('ydr-util').dato;
+var random = require('ydr-util').random;
 var REG_LINES = /[\n\r\t]/g;
 var REG_SPACES = /\s{2,}/g;
-var REG_COMMENTS = /<!--[\s\S]*?-->/g;
+//var REG_COMMENTS = /<!--[\s\S]*?-->/g;
+var REG_PRES = /<pre\b.*?>[\s\S]*?<\/pre>/ig;
 
 
 /**
  * html minify
+ * @param file
  * @param code
  * @param [callback]
  */
 module.exports = function (file, code, callback) {
-    //code = code
-    //    .replace(REG_LINES, ' ')
-    //    .replace(REG_COMMENTS, '')
-    //    .replace(REG_SPACES, ' ');
+    // 保存 <pre>
+    var preMap = {};
+    code = code.replace(REG_PRES, function ($0) {
+        var key = _generateKey();
+        
+        preMap[key] = $0;
+        
+        return key;
+    });
 
+    
+    code = code
+        .replace(REG_LINES, '')
+        .replace(REG_SPACES, ' ');
+
+
+    
+    dato.each(preMap, function (key, val) {
+        code = code.replace(key, val);
+    });
+
+    
+    //console.log(code);
+    
     if (callback) {
         callback(null, code);
     } else {
         return code;
     }
 };
+
+
+function _generateKey() {
+    return ':' + random.string(40, 'aA0') + ':';
+}
+
+
+///////////////////////////////
+var html = '<!--\n\n\ncomment\n\n\n-->\n\n\n      <pre>a\n\n\n</pre>\n\n\n<!--\n\n\ncomment\n\n\n-->\n\n\n      <pre>a\n\n\n</pre>';
+module.exports('', html);
