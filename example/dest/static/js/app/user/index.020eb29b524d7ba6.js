67cb6ee97ed7c7fd4376ba0a940ac54a7a41c7e0
cloudcome <cloudcome@163.com>
2015-04-03 23:49:50 +0800

    支持样式构建相对路径的引用

--- example/dest/static/js/app/user/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/user/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie 1427813059282*/
+/*coolie 1428076150450*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(){"use strict";console.log("libs/all.js")});
