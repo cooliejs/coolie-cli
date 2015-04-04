758b58b0177d00825b67cec01eac1f2bec723898
cloudcome <cloudcome@163.com>
2015-04-04 17:36:14 +0800

    新的构建配置

--- example/dest/static/js/app/user/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/user/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie 1428139740495*/
+/*coolie 1428140164907*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(){"use strict";console.log("libs/all.js")});
