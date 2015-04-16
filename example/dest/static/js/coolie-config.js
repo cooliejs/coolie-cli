832e6efcbe70d5d228d03b5dd4464fa6b708c5e0
cloudcome <cloudcome@163.com>
2015-04-16 14:59:05 +0800

    global.configs

---
+++ example/dest/static/js/coolie-config.js
@@ -0,0 +1,1 @@
+coolie.config({base:"./app/",host:"",version:{"index.js":"020eb29b524d7ba6","user/index.js":"020eb29b524d7ba6"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
