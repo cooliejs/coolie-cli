172757efa0cb21e454bdc2a21c3fb861e4d9c4a3
张云来 <cloudcome@163.com>
2015-05-01 15:28:27 +0800

    0.13.0 + coolie alien

--- build/download-alien.js
+++ build/download-alien.js
@@ -9,7 +9,6 @@
 
 var path = require('path');
 var fs = require('fs');
-var fse = require('fs-extra');
 var request = require('ydr-utils').request;
 var random = require('ydr-utils').random;
 var pkg = require('../package.json');
@@ -71,18 +70,5 @@ module.exports = function (basedir) {
                 log('unzip alien', unzipPath, 'success');
             }
         });
-
-
-        //
-        //stream.pipe(unzip.Extract({
-        //    path: unzipPath
-        //})).on('error', function (err) {
-        //    log('unzip alien', url, 'error');
-        //    log('unzip alien', err.message, 'error');
-        //    process.exit();
-        //}).on('close', function () {
-        //    log('download alien', unzipPath, 'success');
-        //    process.exit();
-        //});
     });
 };
