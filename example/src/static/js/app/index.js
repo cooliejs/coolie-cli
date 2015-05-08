e12ee7e3c72dde1ec98956bf17cd51cc55cb71e5
张云来 <cloudcome@163.com>
2015-05-09 03:32:11 +0800

    require

--- example/src/static/js/app/index.js
+++ example/src/static/js/app/index.js
@@ -1,4 +1,5 @@
 define(function (require, exports, module) {
-require('../../img/100x100.png', 'image');
-require('image!../../img/loading.gif');
+    //require('../../img/100x100.png', 'image');
+    //require('image!../../img/loading.gif');
+    require('../libs/path1/path2/');
 });
