00acb00b420bb1831a0f868c6b0717f2a3c8acb8
张云来 <cloudcome@163.com>
2015-05-08 23:30:42 +0800

    fix path end

--- example/dest/static/js/app/user/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/user/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie@0.14.0 1431078797714*/
+/*coolie@0.14.1 1431099032010*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(l){"use strict";console.log("libs/all.js")});
