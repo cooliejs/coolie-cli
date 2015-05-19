12aa74eff2ba4d6c472d9be3533606628bc58abf
张云来 <cloudcome@163.com>
2015-05-19 13:43:32 +0800

    build coolie-config.js version

--- example/dest/static/js/app/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie@0.15.0 1432013295905*/
+/*coolie@0.15.0 1432014002494*/
 define("0",["1"],function(n,e,f){n("1")});
 define("1",[],function(n){});
--- example/dest/static/js/app/user/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/user/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie@0.14.2 1431113390782*/
+/*coolie@0.15.0 1432014002509*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(l){"use strict";console.log("libs/all.js")});
