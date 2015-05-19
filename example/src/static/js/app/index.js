06ef9843a53cb3f3388e3984b44161fb7aa7081b
张云来 <cloudcome@163.com>
2015-05-20 01:05:13 +0800

    adapt to coolie@0.9.0

--- example/src/static/js/app/index.js
+++ example/src/static/js/app/index.js
@@ -1,5 +1,5 @@
 define(function (require, exports, module) {
-    //require('../../img/100x100.png', 'image');
-    //require('image!../../img/loading.gif');
-    //require('../libs/path1/path2/');
+    require('../../img/100x100.png', 'image');
+    require('image!../../img/loading.gif');
+    require('../libs/path1/path2/');
 });
