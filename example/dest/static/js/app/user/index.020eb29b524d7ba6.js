7f9c45dec13971c0e48c571dcbf39a2bc62d3621
张云来 <cloudcome@163.com>
2015-05-20 00:05:38 +0800

    adapt to coolie@0.9.0

--- example/dest/static/js/app/user/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/user/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie@0.16.0 1432050401357*/
+/*coolie@0.16.0 1432051466009*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(l){"use strict";console.log("libs/all.js")});
--- example/dest/static/js/coolie-config.37fbbfea26df2218.js
+++ example/dest/static/js/coolie-config.37fbbfea26df2218.js
@@ -1,1 +1,1 @@
-/*coolie@0.16.0 1432050401359*/coolie.config({base:"./app/",host:"",debug:!1,version:{"index.js":"74be16979710d4c4","user/index.js":"020eb29b524d7ba6"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
+/*coolie@0.16.0 1432051466012*/coolie.config({base:"./app/",host:"",debug:!1,version:{"index.js":"74be16979710d4c4","user/index.js":"020eb29b524d7ba6"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
