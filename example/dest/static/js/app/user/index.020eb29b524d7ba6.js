1c8e2d4b791e5b487b60197e411554b25efe83a5
张云来 <cloudcome@163.com>
2015-05-19 21:10:16 +0800

    simpleify

--- example/dest/static/js/app/user/index.020eb29b524d7ba6.js
+++ example/dest/static/js/app/user/index.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie@0.15.0 1432022043200*/
+/*coolie@0.15.0 1432040996021*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(l){"use strict";console.log("libs/all.js")});
