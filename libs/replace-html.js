d0122636317e7c13b401ce3b0216d8f59b7100d5
张云来 <cloudcome@163.com>
2015-05-23 20:44:01 +0800

    build icon

--- libs/replace-html.js
+++ libs/replace-html.js
@@ -32,6 +32,7 @@ var buildMap = {};
 var REG_SUFFIX = /(\?.*|#.*)$/;
 var REG_IGNORE = /\bcoolieignore\b/i;
 var REG_EXTNAME = /\.([^.]+)$/;
+var REG_IMAGE_ICON = /icon/;
 
 
 /**
@@ -165,66 +166,104 @@ module.exports = function (file, code) {
         return $1 ? '<link rel="stylesheet" href="' + concat[replaceIndex++].url + '"/>' : $0;
     });
 
+    // <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    // <link rel="apple-touch-icon" href="/apple-touch-icon-72.png" />
+    code = code.replace(REG_LINK, function ($0) {
+        var rel = htmlAttr.get($0, 'rel');
+        var type = htmlAttr.get($0, 'type');
+        var href = htmlAttr.get($0, 'href');
+
+        if (rel === 'shortcut icon' || rel === 'apple-touch-icon' || type === 'image/x-icon') {
+            return _buildResVersion(file, $0, 'href');
+        }
+
+        return $0;
+
+        //if (REG_HTTP.test(href) || !href) {
+        //    return $0;
+        //}
+        //
+        //var absFile;
+        //
+        //try {
+        //    absFile = path.join(srcPath, href);
+        //} catch (err) {
+        //    log('html file', pathURI.toSystemPath(file), 'error');
+        //    log('img', $0, 'error');
+        //    log('img src', href === true ? '<EMPTY>' : href, 'error');
+        //    process.exit();
+        //}
+        //
+        //var basename = path.basename(absFile);
+        //var srcName = basename.replace(REG_SUFFIX, '');
+        //var url = buildMap[absFile];
+        //var version = configs._resVerMap[absFile];
+        //
+
+    });
+
     code = code.replace(REG_IMG, function ($0) {
         if (htmlAttr.get($0, 'coolieignore')) {
             return htmlAttr.remove($0, 'coolieignore');
         }
 
-        var imgSrc = htmlAttr.get($0, 'src');
+        //var imgSrc = htmlAttr.get($0, 'src');
 
-        if (REG_HTTP.test(imgSrc) || !imgSrc) {
-            return $0;
-        }
+        return _buildResVersion(file, $0, 'src');
 
-        var absFile;
-
-        try {
-            absFile = path.join(srcPath, imgSrc);
-        } catch (err) {
-            log('html file', pathURI.toSystemPath(file), 'error');
-            log('img', $0, 'error');
-            log('img src', imgSrc === true ? '<EMPTY>' : imgSrc, 'error');
-            process.exit();
-        }
-
-        var basename = path.basename(absFile);
-        var srcName = basename.replace(REG_SUFFIX, '');
-        var suffix = (basename.match(REG_SUFFIX) || [''])[0];
-
-        absFile = absFile.replace(REG_SUFFIX, '');
-
-        var url = buildMap[absFile];
-        var version = configs._resVerMap[absFile];
-
-        if (!version) {
-            version = encryption.etag(absFile);
-        }
-
-        if (!version) {
-            log('read file', pathURI.toSystemPath(absFile), 'error');
-            process.exit();
-        }
-
-        // 未进行版本构建
-        if (!url) {
-            var extname = path.extname(srcName);
-            var resName = version + extname;
-            var resFile = path.join(destPath, configs.resource.dest, resName);
-
-            try {
-                fs.copySync(absFile, resFile);
-            } catch (err) {
-                log('html file', pathURI.toSystemPath(file), 'error');
-                log('copy from', pathURI.toSystemPath(absFile), 'error');
-                log('copy to', pathURI.toSystemPath(resFile), 'error');
-                log('copy file', err.message, 'error');
-                process.exit();
-            }
-
-            buildMap[absFile] = url = (configs.dest.host ? '' : '/') + path.relative(destPath, resFile);
-        }
-
-        return htmlAttr.set($0, 'src', configs.dest.host + url + suffix);
+        //if (REG_HTTP.test(imgSrc) || !imgSrc) {
+        //    return $0;
+        //}
+        //
+        //var absFile;
+        //
+        //try {
+        //    absFile = path.join(srcPath, imgSrc);
+        //} catch (err) {
+        //    log('html file', pathURI.toSystemPath(file), 'error');
+        //    log('img', $0, 'error');
+        //    log('img src', imgSrc === true ? '<EMPTY>' : imgSrc, 'error');
+        //    process.exit();
+        //}
+        //
+        //var basename = path.basename(absFile);
+        //var srcName = basename.replace(REG_SUFFIX, '');
+        //var suffix = (basename.match(REG_SUFFIX) || [''])[0];
+        //
+        //absFile = absFile.replace(REG_SUFFIX, '');
+        //
+        //var url = buildMap[absFile];
+        //var version = configs._resVerMap[absFile];
+        //
+        //if (!version) {
+        //    version = encryption.etag(absFile);
+        //}
+        //
+        //if (!version) {
+        //    log('read file', pathURI.toSystemPath(absFile), 'error');
+        //    process.exit();
+        //}
+        //
+        //// 未进行版本构建
+        //if (!url) {
+        //    var extname = path.extname(srcName);
+        //    var resName = version + extname;
+        //    var resFile = path.join(destPath, configs.resource.dest, resName);
+        //
+        //    try {
+        //        fs.copySync(absFile, resFile);
+        //    } catch (err) {
+        //        log('html file', pathURI.toSystemPath(file), 'error');
+        //        log('copy from', pathURI.toSystemPath(absFile), 'error');
+        //        log('copy to', pathURI.toSystemPath(resFile), 'error');
+        //        log('copy file', err.message, 'error');
+        //        process.exit();
+        //    }
+        //
+        //    buildMap[absFile] = url = (configs.dest.host ? '' : '/') + path.relative(destPath, resFile);
+        //}
+        //
+        //return htmlAttr.set($0, 'src', configs.dest.host + url + suffix);
     });
 
     return {
@@ -233,3 +272,65 @@ module.exports = function (file, code) {
         mainJS: mainJS
     };
 };
+
+
+/**
+ * 构建资源版本
+ * @param file
+ * @param html
+ * @param attrKey
+ * @returns {String}
+ * @private
+ */
+function _buildResVersion(file, html, attrKey) {
+    var value = htmlAttr.get(html, attrKey);
+
+    if (REG_HTTP.test(value) || !value) {
+        return html;
+    }
+
+    var absFile;
+
+    try {
+        absFile = path.join(configs._srcPath, value);
+    } catch (err) {
+        log('html file', pathURI.toSystemPath(file), 'error');
+        log('img', html, 'error');
+        log('img src', value === true ? '<EMPTY>' : value, 'error');
+        process.exit();
+    }
+
+    var basename = path.basename(absFile);
+    var srcName = basename.replace(REG_SUFFIX, '');
+    var suffix = (basename.match(REG_SUFFIX) || [''])[0];
+
+    absFile = absFile.replace(REG_SUFFIX, '');
+
+    var url = buildMap[absFile];
+    var version = configs._resVerMap[absFile];
+
+    if (!version) {
+        version = encryption.etag(absFile);
+    }
+
+    // 未进行版本构建
+    if (!url) {
+        var extname = path.extname(srcName);
+        var resName = version + extname;
+        var resFile = path.join(configs._destPath, configs.resource.dest, resName);
+
+        try {
+            fs.copySync(absFile, resFile);
+        } catch (err) {
+            log('html file', pathURI.toSystemPath(file), 'error');
+            log('copy from', pathURI.toSystemPath(absFile), 'error');
+            log('copy to', pathURI.toSystemPath(resFile), 'error');
+            log('copy file', err.message, 'error');
+            process.exit();
+        }
+
+        buildMap[absFile] = url = (configs.dest.host ? '' : '/') + path.relative(configs._destPath, resFile);
+    }
+
+    return htmlAttr.set(html, attrKey, configs.dest.host + url + suffix);
+}
