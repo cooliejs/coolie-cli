b1deabd36f76b24b053df661316ddd922c9f3096
张云来 <cloudcome@163.com>
2015-05-23 20:46:40 +0800

    build icon

---
+++ example/dest/static/js/coolie-config.f7e668282426cf623eb64ece35de28b2.js
@@ -0,0 +1,1 @@
+/*coolie@0.16.8 1432385058330*/coolie.config({base:"./app/",host:"",debug:!1,version:{"index.js":"5644d88e9ff08ed5abf99af4e758ac39","user/index.js":"02f56a1613620869833ce8472ebaf4f5"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
