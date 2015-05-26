a3c94f3e96c12629b2b310623666a174b9a2ea7e
张云来 <cloudcome@163.com>
2015-05-26 14:58:05 +0800

    windows path

--- example/dest/static/js/app/user/index.02f56a1613620869833ce8472ebaf4f5.js
+++ example/dest/static/js/app/user/index.02f56a1613620869833ce8472ebaf4f5.js
@@ -1,3 +1,3 @@
-/*coolie@0.16.13 1432623340712*/
+/*coolie@0.16.13 1432623458259*/
 define("0",["1"],function(e){"use strict";e("1"),console.log("app/user/index")});
 define("1",[],function(l){"use strict";console.log("libs/all.js")});
--- example/dest/static/js/coolie-config.f7e668282426cf623eb64ece35de28b2.js
+++ example/dest/static/js/coolie-config.f7e668282426cf623eb64ece35de28b2.js
@@ -1,1 +1,1 @@
-/*coolie@0.16.13 1432623340715*/coolie.config({base:"./app/",host:"",debug:!1,version:{"index.js":"5644d88e9ff08ed5abf99af4e758ac39","user/index.js":"02f56a1613620869833ce8472ebaf4f5"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
+/*coolie@0.16.13 1432623458262*/coolie.config({base:"./app/",host:"",debug:!1,version:{"index.js":"5644d88e9ff08ed5abf99af4e758ac39","user/index.js":"02f56a1613620869833ce8472ebaf4f5"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
