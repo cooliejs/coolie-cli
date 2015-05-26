d40b3390fa684408ab91db08325d59f902d50e83
张云来 <cloudcome@163.com>
2015-05-26 13:33:21 +0800

    up wiki url

--- example/dest/static/js/coolie-config.f7e668282426cf623eb64ece35de28b2.js
+++ example/dest/static/js/coolie-config.f7e668282426cf623eb64ece35de28b2.js
@@ -1,1 +1,1 @@
-/*coolie@0.16.8 1432386620692*/coolie.config({base:"./app/",host:"",debug:!1,version:{"index.js":"5644d88e9ff08ed5abf99af4e758ac39","user/index.js":"02f56a1613620869833ce8472ebaf4f5"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
+/*coolie@0.16.12 1432618388645*/coolie.config({base:"./app/",host:"",debug:!1,version:{"index.js":"5644d88e9ff08ed5abf99af4e758ac39","user/index.js":"02f56a1613620869833ce8472ebaf4f5"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
