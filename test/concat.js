2e90c37bfb5c590175b89fb8f117c2504d3a6686
张云来 <cloudcome@163.com>
2015-06-08 23:48:18 +0800

    script

--- test/concat.js
+++ test/concat.js
@@ -11,6 +11,10 @@ var path = require('path');
 var file = path.join(__dirname, '../example/src/html/index3.html');
 var concat = require('../libs/concat.js');
 
+global.configs = {
+    _srcPath: path.join(__dirname, '../example/src/'),
+    _jsPath: path.join(__dirname, '../example/src/static/js/')
+};
 
 var ret = concat(file, '<script src="../static/js/index3-1.js"></script>\
     <script src="../static/js/index3-2.js"></script>', {
