a8312fd35192e80058e4692a4450705f15b1d729
张云来 <cloudcome@163.com>
2015-06-09 21:55:07 +0800

    concat

---
+++ example/dest/static/js/7e978e956a05ae808c817ee8dbd69a4b.js
@@ -0,0 +1,3 @@
+/*coolie@0.17.2 1433857128098*/
+!function(){var a=1;alert(a)}();
+!function(){var a=2;alert(a)}();
---
+++ example/dest/static/js/coolie-config.a4400fa72a67edc5f86720da6b0bf8cf.js
@@ -0,0 +1,2 @@
+/*coolie@0.17.2 1433857127997*/
+coolie.config({base:"./app/",debug:!1,cache:!0,version:{"index.js":"cb828e2036189f6c737096563e82da17","user/index.js":"fc6902957b23c990e7fa6fe204909523"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
