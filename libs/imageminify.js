9f1a2becefc85ab32a3ca8f474ad7bf4323313a4
张云来 <cloudcome@163.com>
2015-06-10 19:53:50 +0800

    rm build image

--- libs/imageminify.js
+++ libs/imageminify.js
@@ -7,12 +7,12 @@
 
 'use strict';
 
-var optimage = require('optimage');
-var path = require('path');
-var fse = require('fs-extra');
-var log = require('./log.js');
-var pathURI = require('./path-uri.js');
-var number = require('ydr-utils').number;
+//var optimage = require('optimage');
+//var path = require('path');
+//var fse = require('fs-extra');
+//var log = require('./log.js');
+//var pathURI = require('./path-uri.js');
+//var number = require('ydr-utils').number;
 
 
 /**
@@ -21,22 +21,22 @@ var number = require('ydr-utils').number;
  * @param callback {Function} 异步回调
  */
 module.exports = function (inputFile, callback) {
-    var configs = global.configs;
-    var originalSize = fse.statSync(inputFile).size;
-    var outputFile = configs._resImageMap[inputFile];
-
-    fse.ensureFileSync(outputFile);
-    optimage({
-        inputFile: inputFile,
-        outputFile: outputFile
-    }, function (err, res) {
-        //if (err) {
-        //    log('imageminify', pathURI.toSystemPath(inputFile), 'error');
-        //    log('imageminify', err.message, 'error');
-        //}
-
-        var saved = number.parseFloat(res && res.saved, 0);
-
-        callback(null, originalSize - saved, originalSize);
-    });
+    //var configs = global.configs;
+    //var originalSize = fse.statSync(inputFile).size;
+    //var outputFile = configs._resImageMap[inputFile];
+    //
+    //fse.ensureFileSync(outputFile);
+    //optimage({
+    //    inputFile: inputFile,
+    //    outputFile: outputFile
+    //}, function (err, res) {
+    //    //if (err) {
+    //    //    log('imageminify', pathURI.toSystemPath(inputFile), 'error');
+    //    //    log('imageminify', err.message, 'error');
+    //    //}
+    //
+    //    var saved = number.parseFloat(res && res.saved, 0);
+    //
+    //    callback(null, originalSize - saved, originalSize);
+    //});
 };
