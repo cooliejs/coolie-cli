aff312982a4c57dbff40a5dec02aad7f0cf00a9f
张云来 <cloudcome@163.com>
2015-06-11 16:50:54 +0800

    join uri path

--- example/dest/static/js/7e978e956a05ae808c817ee8dbd69a4b.js
+++
@@ -1,3 +0,0 @@
-/*coolie@0.20.3*/
-!function(){var a=1;alert(a)}();
-!function(){var a=2;alert(a)}();
--- example/dest/static/js/coolie-config.a4400fa72a67edc5f86720da6b0bf8cf.js
+++
@@ -1,2 +0,0 @@
-/*coolie@0.20.3*/
-coolie.config({base:"./app/",debug:!1,cache:!0,version:{"index.js":"cb828e2036189f6c737096563e82da17","user/index.js":"fc6902957b23c990e7fa6fe204909523"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
