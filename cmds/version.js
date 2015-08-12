728f5a15d4e8cf5a87e081bb6ad496cc45231299
张云来 <cloudcome@163.com>
2015-08-12 10:15:24 +0800

    cmd version

--- cmds/version.js
+++ cmds/version.js
@@ -7,6 +7,62 @@
 
 'use strict';
 
-var fs = require('fs');
+var request = require('ydr-utils').request;
+var log = require('../libs/log.js');
+var pkg = require('../package.json');
+var howdo = require('howdo');
+var currentVersion = pkg.version;
+var coolieCliURL = 'http://registry.npmjs.com/coolie';
+var coolieJSURL = 'https://raw.githubusercontent.com/cloudcome/coolie/master/package.json';
+
+module.exports = function(){
+    log('local version', currentVersion, 'success');
+    log('check version', 'wait a moment...');
+
+    howdo
+        // 检查 coolie cli 版本
+        .task(function () {
+            request.get(coolieCliURL, function (err, data) {
+                if(err){
+                    log('check version', 'connect npmjs.com error', 'error');
+                    process.exit(1);
+                }
+
+                var json = {};
+
+                try{
+                    json = JSON.parse(data);
+                }catch(err){
+                    log('check version', 'parse json string error', 'error');
+                    process.exit(1);
+                }
+
+                log('coolie.cli', json['dist-tags'].latest, 'success');
+            });
+        })
+        // 检查 coolie.js 版本
+        .task(function () {
+            request.get(coolieJSURL, function (err, data) {
+                if(err){
+                    log('check version', 'connect github.com error', 'error');
+                    process.exit(1);
+                }
+
+                var json = {};
+
+                try{
+                    json = JSON.parse(data);
+                }catch(err){
+                    log('check version', 'parse json string error', 'error');
+                    process.exit(1);
+                }
+
+                log('coolie.js', json.version, 'success');
+            });
+        })
+        .together(function () {
+            //
+        });
+};
 
 
