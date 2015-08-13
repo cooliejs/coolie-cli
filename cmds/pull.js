e66f057e7411cca73d91c00ff5eb64a6d1fee78e
张云来 <cloudcome@163.com>
2015-08-13 11:15:14 +0800

    cmd json

--- cmds/pull.js
+++ cmds/pull.js
@@ -7,6 +7,39 @@
 
 'use strict';
 
+var path = require('path');
 var fs = require('fs');
+var request = require('ydr-utils').request;
+var pkg = require('../package.json');
+var log = require('../libs/log.js');
 
+module.exports = function (basedir) {
+    var writeFile = path.join(basedir, "./coolie.min.js");
+    var writeStream = fs.createWriteStream(writeFile);
+    var url = pkg.coolie;
+
+    log('pull coolie.min.js', url);
+    request.down(pkg.coolie, function (err, stream, res) {
+        if (err) {
+            log('pull coolie.min.js', url, 'error');
+            log('pull coolie.min.js', err.message, 'error');
+            process.exit(1);
+        }
+
+        if (res.statusCode !== 200) {
+            log('download alien', url, 'error');
+            log('download alien', 'response statusCode is ' + res.statusCode, 'error');
+            return process.exit(1);
+        }
+
+        stream.pipe(writeStream).on('error', function (err) {
+            log('pull coolie.min.js', url, 'error');
+            log('pull coolie.min.js', err.message, 'error');
+            process.exit(1);
+        }).on('close', function () {
+            log('pull coolie.min.js', writeFile, 'success');
+            process.exit(1);
+        });
+    });
+};
 
