47d2f134baa17e806df18d8cb27fe28a6c0a0e11
张云来 <cloudcome@163.com>
2015-08-13 11:36:57 +0800

    cmd pull

--- cmds/pull.js
+++ cmds/pull.js
@@ -1,5 +1,5 @@
 /*!
- * 文件描述
+ * cmd pull
  * @author ydr.me
  * @create 2015-08-12 09:18
  */
@@ -8,35 +8,42 @@
 'use strict';
 
 var path = require('path');
-var fs = require('fs');
+var fse = require('fs-extra');
 var request = require('ydr-utils').request;
 var pkg = require('../package.json');
 var log = require('../libs/log.js');
+var REG_VERSION = /^ \* @version ([\d.]+)/m;
 
 module.exports = function (basedir) {
-    var writeFile = path.join(basedir, "./coolie.min.js");
-    var writeStream = fs.createWriteStream(writeFile);
     var url = pkg.coolie;
 
     log('pull coolie.min.js', url);
-    request.down(pkg.coolie, function (err, stream, res) {
+    request.get({
+        url: pkg.coolie
+    }, function (err, body, res) {
         if (err) {
             log('pull coolie.min.js', url, 'error');
             log('pull coolie.min.js', err.message, 'error');
-            process.exit(1);
-        }
-
-        if (res.statusCode !== 200) {
-            log('download alien', url, 'error');
-            log('download alien', 'response statusCode is ' + res.statusCode, 'error');
             return process.exit(1);
         }
 
-        stream.pipe(writeStream).on('error', function (err) {
+        if (res.statusCode !== 200) {
             log('pull coolie.min.js', url, 'error');
-            log('pull coolie.min.js', err.message, 'error');
-            process.exit(1);
-        }).on('close', function () {
+            log('pull coolie.min.js', 'response statusCode is ' + res.statusCode, 'error');
+            return process.exit(1);
+        }
+
+
+        var version = (body.match(REG_VERSION) || ['', '0.0.0'])[1];
+        var writeFile = path.join(basedir, './coolie.min-' + version + '.js');
+
+        fse.outputFile(writeFile, body, function (err) {
+            if (err) {
+                log('pull coolie.min.js', url, 'error');
+                log('pull coolie.min.js', err.message, 'error');
+                return process.exit(1);
+            }
+
             log('pull coolie.min.js', writeFile, 'success');
             process.exit(1);
         });
