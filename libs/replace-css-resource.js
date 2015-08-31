2bb5d98037762da45e5783a7b7b7722334926dc6
张云来 <cloudcome@163.com>
2015-08-31 16:04:05 +0800

    htmlminfiy

--- libs/replace-css-resource.js
+++ libs/replace-css-resource.js
@@ -12,7 +12,7 @@ var fs = require('fs-extra');
 var log = require('./log.js');
 var pathURI = require('./path-uri.js');
 var base64 = require('./base64.js');
-var encryption = require('ydr-utils').encryption;
+var copy = require('./copy.js');
 var REG_URL = /url\s*?\((.*?)\)/ig;
 var REG_QUOTE = /^["']|['"]$/g;
 
@@ -22,10 +22,9 @@ var REG_QUOTE = /^["']|['"]$/g;
  * @param file {String} 待替换的文件
  * @param css {String} 待替换的 CSS 文件
  * @param destCSSFile {String} CSS 文件的保存路径
- * @param [base64Resource=false] {Boolean} 是否替换为 base64 编码
  * @returns {String}
  */
-module.exports = function (file, css, destCSSFile, base64Resource) {
+module.exports = function (file, css, destCSSFile) {
     var configs = global.configs;
 
     return css.replace(REG_URL, function ($0, $1) {
@@ -38,57 +37,12 @@ module.exports = function (file, css, destCSSFile, base64Resource) {
         }
 
         var absDir = pathURI.isRelativeFile(pathRet.path) ? path.dirname(file) : configs._srcPath;
-        var absFile;
-
-        try {
-            absFile = path.join(absDir, pathRet.path);
-        } catch (err) {
-            log('replace resource', pathURI.toSystemPath(file), 'error');
-            log('replace resource', $0, 'error');
-            log('replace resource', err.message, 'error');
-            process.exit(1);
-        }
-
-        if (base64Resource) {
-            var b64 = configs._resBase64Map[absFile];
-
-            if (!b64) {
-                configs._resBase64Map[absFile] = b64 = base64(absFile);
-            }
-
-            return 'url(' + b64 + ')';
-        }
-
-        var version = configs._resVerMap[absFile];
-        var destFile = configs._resDestMap[absFile];
-
-        if (!version) {
-            version = encryption.etag(absFile).slice(0, configs.dest.versionLength);
-
-            //var isImage = pathURI.isImage(extname);
-
-            destFile = path.join(configs._destPath, configs.resource.dest, version + pathRet.extname);
-
-            //if (configs.resource.minify !== false && isImage) {
-            //    if (!configs._resImageMap[absFile]) {
-            //        configs._resImageMap[absFile] = destFile;
-            //        configs._resImageList.push(absFile);
-            //    }
-            //} else if (configs.resource.minify === false || !isImage) {
-            try {
-                fs.copySync(absFile, destFile);
-            } catch (err) {
-                log('copy file', pathURI.toSystemPath(file), 'error');
-                log('copy from', pathURI.toSystemPath(absFile), 'error');
-                log('copy to', pathURI.toSystemPath(destFile), 'error');
-                log('copy file', err.message, 'error');
-                process.exit(1);
-            }
-            //}
-
-            configs._resVerMap[absFile] = version;
-            configs._resDestMap[absFile] = destFile;
-        }
+        var absFile = path.join(absDir, pathRet.path);
+        var destFile = copy(absFile, {
+            dest: configs._resDestPath,
+            version: true,
+            logType: 1
+        });
 
         var url = '';
 
