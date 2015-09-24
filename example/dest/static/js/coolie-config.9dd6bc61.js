8d7a87e65a1886aae0a5d624db2b24c3d59f856a
张云来 <cloudcome@163.com>
2015-09-24 10:17:25 +0800

    fix <img src data-original>

--- example/dest/static/js/coolie-config.9dd6bc61.js
+++ example/dest/static/js/coolie-config.9dd6bc61.js
@@ -1,2 +1,2 @@
-/*coolie@0.22.9*/
+/*coolie@0.22.10*/
 coolie.config({base:"./app/",debug:!1,cache:!0,version:{"index.js":"335fed0c","user/index.js":"0b384aca","0.js":"23266c74"}}).use().callback(function(){alert(123)}).callback(function(){alert(456)});
