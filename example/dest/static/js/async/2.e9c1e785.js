e1e79d7d898ac5e0f44e73148db3b831a2b20bde
张云来 <cloudcome@163.com>
2015-10-09 16:01:22 +0800

    require.async

---
+++ example/dest/static/js/async/2.e9c1e785.js
@@ -0,0 +1,2 @@
+/*coolie@0.23.0*/
+define("0",[],function(e,n,t){document.getElementById("ret").innerHTML="page2"});
---
+++ example/dest/static/js/coolie-config.5cfc2b4f.js
@@ -0,0 +1,2 @@
+/*coolie@0.23.0*/
+coolie.config({base:"./app",async:"../async/",chunk:"../chunk/",debug:!1,cache:!0,version:{"async.js":"4423be68","../async/1.js":"c096923f","../async/2.js":"e9c1e785","../async/3.js":"621c2be0"}}).use();
