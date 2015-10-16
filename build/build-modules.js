9d5dcf0751a6501d17fed9ed25f66e81ae76b7cf
cloudcome <cloudcome@163.com>
2015-10-16 16:35:08 +0800

    coolie api

--- build/build-modules.js
+++ build/build-modules.js
@@ -57,7 +57,7 @@ module.exports = function (srcPath) {
 
     configs.srcDirname = srcPath;
     configs.destDirname = destPath;
-    configs._jsPath = jsPath;
+    configs.destJSDirname = jsPath;
     configs._mainFiles = {};
     configs._bufferMap = {};
     configs._fileIdMap = {};
