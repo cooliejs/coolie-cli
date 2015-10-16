927bf0251823673977d740e2c303d1afdf27e702
cloudcome <cloudcome@163.com>
2015-10-16 16:40:06 +0800

    coolie api

--- libs/replace-html.js
+++ libs/replace-html.js
@@ -107,7 +107,7 @@ module.exports = function (file, code) {
         if (hasCoolie) {
             var copySrc = copy(src, {
                 srcFile: file,
-                dest: configs._jsDestPath,
+                dest: configs.destJSPath,
                 srcCode: $0,
                 version: true,
                 logType: 1
