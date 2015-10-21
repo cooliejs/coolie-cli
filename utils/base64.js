ca0aad0fb81c62c589820f5f44e72d01c7b36da0
cloudcome <cloudcome@163.com>
2015-10-21 17:44:35 +0800

    coolie api

---
+++ utils/base64.js
@@ -0,0 +1,76 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-05-08 10:59
+ */
+
+
+'use strict';
+
+var fs = require('fs');
+var path = require('ydr-utils').path;
+var log = require('./log.js');
+var pathURI = require('./path-uri.js');
+var dato = require('ydr-utils').dato;
+var mime = require('ydr-utils').mime;
+var typeis = require('ydr-utils').typeis;
+
+
+/**
+ * 文件 base64 编码
+ * @param file {String|Object} 文件或者文件二进制编码
+ * @param [extname]
+ * @param [callback]
+ * @returns {*}
+ */
+module.exports = function (file, extname, callback) {
+    var args = arguments;
+
+    if (args.length === 2) {
+        // file, callback
+        if (typeis.function(args[1])) {
+            callback = args[1];
+            extname = null;
+        } else {
+            callback = null;
+        }
+    }
+
+    var binary;
+
+    if (typeis.string(file)) {
+        extname = extname || path.extname(file);
+
+        try {
+            binary = fs.readFileSync(file, 'binary');
+        } catch (err) {
+            log('base64', pathURI.toSystemPath(file), 'error');
+            log('read file', pathURI.toSystemPath(file), 'error');
+            log('read file', err.message, 'error');
+            process.exit(1);
+        }
+    } else {
+        binary = file;
+    }
+
+    // data:image/png;base64,
+    var prefix = 'data:' + mime.get(extname) + ';base64,';
+
+    var base64;
+
+    try {
+        base64 = new Buffer(binary, 'binary').toString('base64');
+    } catch (err) {
+        log('base64 file', pathURI.toSystemPath(file), 'error');
+        log('base64 error', err.message, 'error');
+        process.exit(1);
+    }
+
+    if (callback) {
+        callback(null, prefix + base64);
+    } else {
+        return prefix + base64;
+    }
+};
+
+
---
+++ utils/hook.js
@@ -0,0 +1,63 @@
+/**
+ * 挂载（外挂）
+ * @author ydr.me
+ * @create 2015-10-19 14:18
+ */
+
+
+'use strict';
+
+var dato = require('ydr-utils').dato;
+var typeis = require('ydr-utils').typeis;
+
+var callbacks = {};
+
+
+/**
+ * 注册挂载
+ * @param type
+ * @returns {Function}
+ */
+exports.bind = function (type) {
+    return function (callback) {
+        callbacks[type] = callbacks[type] || [];
+
+        if (typeis.function(callback)) {
+            callbacks[type].push(callback);
+        }
+    };
+};
+
+
+/**
+ * 执行挂载
+ * @param type
+ * @returns {undefined}
+ */
+exports.exec = function (type/*arguments*/) {
+    var coolie = global.coolie;
+    var list = callbacks[type];
+    var ret = undefined;
+    var args = dato.toArray(arguments).slice(1);
+
+    list = list || [];
+    dato.each(list, function (index, callback) {
+        var _ret = callback.apply(coolie, args);
+
+        // 如果返回 false，则终端
+        if (_ret === false) {
+            ret = _ret;
+            return ret;
+        }
+
+        if (typeis.undefined(_ret)) {
+            return;
+        }
+
+        ret = _ret;
+        return false;
+    });
+
+    return ret;
+};
+
