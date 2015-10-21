6069e6e6446c3fc554457392589d03c457dde0c4
cloudcome <cloudcome@163.com>
2015-10-21 18:46:08 +0800

    coolie api

--- utils/copy.js
+++ utils/copy.js
@@ -10,10 +10,14 @@
 var dato = require('ydr-utils').dato;
 var typeis = require('ydr-utils').typeis;
 var encryption = require('ydr-utils').encryption;
-var pathURI = require('./path-uri.js');
-var log = require('./log.js');
 var path = require('ydr-utils').path;
 var fse = require('fs-extra');
+
+var log = require('./log.js');
+var pathURI = require('./path-uri.js');
+
+var copyFilesMap = {};
+var copyLength = 0;
 var REG_POINT = path.sep === '/' ? /^\.{1,2}\// : /^\.{1,2}\\/;
 var defaults = {
     // 所在的原始文件
@@ -30,7 +34,10 @@ var defaults = {
     // 0：不打印
     // 1：源文件
     // 2：目标文件
-    logType: 2
+    logType: 2,
+    srcDirname: '',
+    destDirname: '',
+    versionLength: 32
 };
 
 /**
@@ -45,12 +52,11 @@ module.exports = function (file, options) {
 
     options = dato.extend({}, defaults, options);
 
-    var configs = global.configs;
-    var fromTo = pathURI.relative(configs.srcDirname, file);
+    var fromTo = pathURI.relative(options.srcDirname, file);
 
     if (REG_POINT.test(fromTo)) {
         if (pathURI.isRelativeRoot(file)) {
-            file = path.join(configs.srcDirname, file);
+            file = path.join(options.srcDirname, file);
         } else if (pathURI.isRelativeFile(file) && options.srcFile) {
             file = path.join(path.dirname(options.srcFile), file);
         } else if (options.dest) {
@@ -71,43 +77,43 @@ module.exports = function (file, options) {
         process.exit(1);
     }
 
-    var toFile = configs._copyFilesMap[file];
+    var toFile = copyFilesMap[file];
 
     if (toFile) {
         return toFile;
     }
 
-    var releativeTo = pathURI.relative(configs.srcDirname, file);
+    var releativeTo = pathURI.relative(options.srcDirname, file);
 
     if (options.version) {
-        var version = encryption.etag(file).slice(0, configs.dest.versionLength);
+        var version = encryption.etag(file).slice(0, options.versionLength);
         var extname = path.extname(file);
 
         if (options.dest) {
             toFile = path.join(options.dest, version + extname);
         } else {
-            toFile = path.join(configs.destDirname, path.dirname(releativeTo), version + extname);
+            toFile = path.join(options.destDirname, path.dirname(releativeTo), version + extname);
         }
     } else {
         if (options.dest) {
             toFile = path.join(options.dest, releativeTo);
         } else {
-            toFile = path.join(configs.destDirname, releativeTo);
+            toFile = path.join(options.destDirname, releativeTo);
         }
     }
 
     try {
         fse.copySync(file, toFile);
-        configs._copyFilesMap[file] = toFile;
-        configs._copyLength++;
+        copyFilesMap[file] = toFile;
+        copyLength++;
 
         switch (options.logType) {
             case 1:
-                log('√', pathURI.toRootURL(file, configs.srcDirname), 'success');
+                log('√', pathURI.toRootURL(file, options.srcDirname), 'success');
                 break;
 
             case 2:
-                log('√', pathURI.toRootURL(toFile, configs.srcDirname), 'success');
+                log('√', pathURI.toRootURL(toFile, options.srcDirname), 'success');
                 break;
         }
     } catch (err) {
@@ -119,3 +125,13 @@ module.exports = function (file, options) {
 
     return toFile;
 };
+
+
+/**
+ * 获取复制文件的个数
+ * @returns {number}
+ */
+module.exports.getCopLength = function () {
+    return copyLength;
+};
+
