8a83e149ebe20fc5d5574200921f6915acb10dcd
cloudcome <cloudcome@163.com>
2015-10-21 18:50:11 +0800

    coolie api

--- test/utils/copy.js
+++ test/utils/copy.js
@@ -7,7 +7,20 @@
 
 'use strict';
 
+var path = require('path');
+var fs = require('fs');
+
+var copy = require('../../utils/copy.js');
+var file = path.join(__dirname, '../../example/src/html/replace.html');
+
+//var code = fs.readFileSync(file, 'utf8');
+var srcDirname = path.join(__dirname, '../../example/src/');
+var destDirname = path.join(__dirname, '../../example/dest/');
+//var destResourceDirname = path.join(destDirname, 'res');
 
 
-
+copy(file, {
+    srcDirname: srcDirname,
+    destDirname: destDirname
+});
 
