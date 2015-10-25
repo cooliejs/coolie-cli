81d953622517e656c4e92953c56db5ac50dc9064
cloudcome <cloudcome@163.com>
2015-10-25 14:06:39 +0800

    coolie api

--- test/utils/base64.js
+++ test/utils/base64.js
@@ -14,8 +14,23 @@ var base64 = require('../../utils/base64.js');
 
 var base64File = path.join(__dirname, './base64.js');
 
-console.log(base64('1'));
-console.log(base64(base64File));
+describe('utils/base64', function () {
+    it('base64.string', function () {
+        var ret1 = base64.string('1');
+        var ret2 = base64.string('1', '.txt');
+
+        assert.equal(ret1, 'MQ==');
+        assert.equal(ret2, 'data:text/plain;base64,MQ==');
+    });
+
+    it('base64.file', function () {
+        var ret1 = base64.file(base64File);
+        var ret2 = base64.file(base64File, '.css');
+
+        assert.equal(/data:text\/javascript;base64,/.test(ret1), true);
+        assert.equal(/data:text\/css;base64,/.test(ret2), true);
+    });
+});
 
 
 
