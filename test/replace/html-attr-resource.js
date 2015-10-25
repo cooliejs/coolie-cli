2298f560710b210bd1257fa6be253e2928967848
cloudcome <cloudcome@163.com>
2015-10-25 15:40:34 +0800

    coolie api

--- test/replace/html-attr-resource.js
+++ test/replace/html-attr-resource.js
@@ -9,6 +9,7 @@
 
 var path = require('path');
 var fs = require('fs');
+var assert = require('assert');
 
 var replaceHTMLAttrResource = require('../../replace/html-attr-resource.js');
 var file = path.join(__dirname, '../../example/src/html/replace.html');
@@ -18,14 +19,22 @@ var srcDirname = path.join(__dirname, '../../example/src/');
 var destDirname = path.join(__dirname, '../../example/dest/');
 var destResourceDirname = path.join(destDirname, 'res');
 
-var ret = replaceHTMLAttrResource(file, {
-    versionLength: 16,
-    srcDirname: srcDirname,
-    destDirname: destDirname,
-    destHost: '/',
-    destResourceDirname: destResourceDirname,
-    code: code
+describe('replace/html-attr-resource.js', function () {
+    it('e', function () {
+        var ret = replaceHTMLAttrResource(file, {
+            versionLength: 16,
+            srcDirname: srcDirname,
+            destDirname: destDirname,
+            destHost: '/',
+            destResourceDirname: destResourceDirname,
+            code: code
+        });
+        var REG_LINK = /<link[\s\S]*?href="\/res\/.*\.ico"/;
+        var REG_IMG = /<img[\s\S]*?src="\/res\/.*\.ico"/;
+
+        assert.equal(REG_LINK.test(ret), true);
+        assert.equal(REG_IMG.test(ret), true);
+    });
 });
 
-console.log(ret);
 
