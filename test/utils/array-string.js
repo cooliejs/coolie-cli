f2a5ee0dedcc53103d6ab2e676def82248a8e176
cloudcome <cloudcome@163.com>
2015-10-28 00:29:21 +0800

    coolie api

---
+++ test/utils/array-string.js
@@ -0,0 +1,28 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-10-28 00:25
+ */
+
+
+'use strict';
+
+var assert = require('assert');
+
+var arrayString = require('../../utils/array-string.js');
+
+
+describe('utils/array-string.js', function () {
+    var ret1 = ['a', '1', 2];
+    var ret2 = arrayString.stringify(ret1);
+    var ret3 = arrayString.parse(ret2);
+
+    it('stringify', function () {
+        assert.equal(ret2, '["a","1","2"]');
+    });
+
+    it('parse', function () {
+        assert.deepEqual(ret3, ['a', '1', '2']);
+    });
+});
+
