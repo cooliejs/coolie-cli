799b41f9623716063952f392222667be6b455089
cloudcome <cloudcome@163.com>
2015-10-31 20:29:56 +0800

    coolie api

--- cmds/init.js
+++ cmds/init.js
@@ -7,16 +7,41 @@
 
 'use strict';
 
+var howdo = require('howdo');
 var fs = require('fs');
 var debug = require('ydr-utils').debug;
 var typeis = require('ydr-utils').typeis;
 var path = require('ydr-utils').path;
 
-var coolieConfigJSPath = path.join(__dirname, '../data/coolie.config.js');
 
-var writeFile = function (name, destDirname) {
+/**
+ * 生成文件
+ * @param name {String} 文件名
+ * @param destDirname {String} 目标地址
+ * @param callback {Function} 回调
+ * @returns {*}
+ */
+var writeFile = function (name, destDirname, callback) {
+    var destPath = path.join(destDirname, name);
+    var srcPath = path.join(__dirname, '../data/', name);
 
-}
+    if (typeis.file(destPath)) {
+        debug.error('init error', path.toSystem(destPath) + ' is exist');
+        return callback();
+    }
+
+    fs.createReadStream(srcPath)
+        .pipe(fs.createWriteStream(destPath))
+        .on('error', function (err) {
+            debug.error(name, path.toSystem(destPath));
+            debug.error('init error', err.message);
+            process.exit(1);
+        })
+        .on('close', function () {
+            debug.success('init success', path.toSystem(destPath));
+            callback();
+        });
+};
 
 /**
  * 生成配置文件
@@ -24,22 +49,14 @@ var writeFile = function (name, destDirname) {
  * @param options.destDirname {String} 根目录
  */
 module.exports = function (options) {
-    var destPath = path.join(options.destDirname, 'coolie.config.js');
-
-    if (typeis.file(destPath)) {
-        debug.error('init error', path.toSystem(destPath) + ' is exist');
-        return process.exit(1);
-    }
-
-    fs.createReadStream(coolieConfigJSPath)
-        .pipe(fs.createWriteStream(destPath))
-        .on('error', function (err) {
-            debug.error('coolie.config.js', path.toSystem(destPath));
-            debug.error('init error', err.message);
-            process.exit(1);
+    howdo
+        .task(function (done) {
+            writeFile('coolie.config.js', options.destDirname, done);
         })
-        .on('close', function () {
-            debug.success('init success', path.toSystem(destPath));
+        .task(function (done) {
+            writeFile('coolie-config.js', options.destDirname, done);
+        })
+        .together(function () {
             process.exit(1);
         });
 };
