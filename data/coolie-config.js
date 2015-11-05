1833c4753a70531bc11e1f53f883bf1e177d4e82
cloudcome <cloudcome@163.com>
2015-11-06 00:48:29 +0800

    coolie config template

--- data/coolie-config.js
+++ data/coolie-config.js
@@ -1,6 +1,11 @@
 /**
+ * ======================================================
+ * coolie.js 配置文件 `coolie-config.js`
+ * 使用 `coolie.init -j` 生成 `coolie-config.js` 文件模板
  * 前端模块加载器配置文件
+ * @link http://coolie.ydr.me/introduction/coolie-config.js/
  * @author ydr.me
+ * ======================================================
  */
 
 coolie.config({
