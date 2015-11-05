0e206bfd53b815ba6dda7cb76d5600032678d6db
cloudcome <cloudcome@163.com>
2015-11-05 11:27:44 +0800

    coolie api

--- cmds/banner.js
+++ cmds/banner.js
@@ -15,9 +15,10 @@ var string = require('ydr-utils').string;
 var pkg = require('../package.json');
 
 module.exports = function () {
-    console.log('╔═══════════════════════════════════════════════════╗');
-    console.log('║  ', 'coolie@' + string.padRight(pkg.version, 8, ' '), '                                ║');
-    console.log('║  ', pkg.description, '             ║');
-    console.log('╚═══════════════════════════════════════════════════╝');
-    console.log('');
+    console.log();
+    console.log('╔══════════════════════════════════════════════════════╗');
+    console.log('║  ', 'coolie@' + string.padRight(pkg.version, 8, ' '), '                                   ║');
+    console.log('║  ', pkg.description, '                ║');
+    console.log('╚══════════════════════════════════════════════════════╝');
+    console.log();
 };
