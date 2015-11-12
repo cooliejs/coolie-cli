cea4cc4fb89598d6c0ea6714b67b86a9a30e38e7
cloudcome <cloudcome@163.com>
2015-11-12 16:21:41 +0800

    fix html minify options

---
+++ example/dest/static/js/async/2.e9c1e78519704801dc6b976f8a8d6d55.js
@@ -0,0 +1,2 @@
+/*coolie@1.0.7*/
+define("0",[],function(e,n,t){document.getElementById("ret").innerHTML="page2"});
--- example/src/coolie.config.js
+++ example/src/coolie.config.js
@@ -20,7 +20,7 @@ module.exports = function (coolie) {
         },
         "html": {
             "src": [
-                "./html/index.html"
+                "./html/**"
             ],
             "minify": false
         },
