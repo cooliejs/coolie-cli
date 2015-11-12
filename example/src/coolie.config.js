7cc34775c59958f7ae4594d18d64e1266b2965f4
cloudcome <cloudcome@163.com>
2015-11-12 14:58:49 +0800

    fix html minify options

--- example/src/coolie.config.js
+++ example/src/coolie.config.js
@@ -48,32 +48,32 @@ module.exports = function (coolie) {
 
 
     // <img data-original="/img.png"> 引用资源替换
-    coolie.use(function (options) {
-        var REG_IMG = /<img[\s\S]*?>(?!["'])/gi;
-
-        options.code = options.code.replace(REG_IMG, function (htmlTag) {
-            // 读取 data-original 属性
-            var dataOriginal = coolie.utils.getHTMLTagAttr(htmlTag, 'data-original');
-
-            // 属性不为空或者属性为 true
-            if (!dataOriginal || dataOriginal === true) {
-                return;
-            }
-
-            // 转换为绝对文件地址
-            var dataOriginalFile = coolie.utils.getAbsolutePath(dataOriginal, options.file);
-
-            if (!dataOriginalFile) {
-                return htmlTag;
-            }
-
-            // 复制文件
-            var toURI = coolie.utils.copyResourceFile(dataOriginalFile);
-
-            // 重设 data-original 属性值
-            return coolie.utils.setHTMLTagAttr(htmlTag, 'data-original', toURI);
-        });
-
-        return options;
-    });
+    //coolie.use(function (options) {
+    //    var REG_IMG = /<img[\s\S]*?>(?!["'])/gi;
+    //
+    //    options.code = options.code.replace(REG_IMG, function (htmlTag) {
+    //        // 读取 data-original 属性
+    //        var dataOriginal = coolie.utils.getHTMLTagAttr(htmlTag, 'data-original');
+    //
+    //        // 属性不为空或者属性为 true
+    //        if (!dataOriginal || dataOriginal === true) {
+    //            return;
+    //        }
+    //
+    //        // 转换为绝对文件地址
+    //        var dataOriginalFile = coolie.utils.getAbsolutePath(dataOriginal, options.file);
+    //
+    //        if (!dataOriginalFile) {
+    //            return htmlTag;
+    //        }
+    //
+    //        // 复制文件
+    //        var toURI = coolie.utils.copyResourceFile(dataOriginalFile);
+    //
+    //        // 重设 data-original 属性值
+    //        return coolie.utils.setHTMLTagAttr(htmlTag, 'data-original', toURI);
+    //    });
+    //
+    //    return options;
+    //});
 };
