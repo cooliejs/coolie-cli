db1f950027e0ab048124d6e4ee63d06cfa877569
cloudcome <cloudcome@163.com>
2015-11-27 20:58:14 +0800

    fix：dest coolie config base dirname

---
+++ example/dest/static/js/58a9223ee30dfa05dd25e359dad398e0.js
@@ -0,0 +1,8 @@
+/*coolie@1.0.11*/
+!function(){var a=1;alert(a);alert(a+2)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
---
+++ example/dest/static/js/app/e8c4ad83382520d4cf4e619ca0a8ca23.js
@@ -0,0 +1,4 @@
+/*coolie@1.0.11*/
+define("0",["c","b"],function(t,e,s){"use strict";t("c");t("b")});
+define("c",[],function(y,d,r){r.exports="data:text/html;base64,IDxpbWcgc3JjPSIvc3RhdGljL3Jlcy9mMWY5YjhkZmMxYTM5YTBiMjM2ODQ5Yjk0MTM5MzU2NS5qcGciICBhbHQ9ImdnZ2dnIi8+PGltZyBzcmM9Ii9zdGF0aWMvcmVzL2YxZjliOGRmYzFhMzlhMGIyMzY4NDliOTQxMzkzNTY1LmpwZz9hPTIiICBhbHQ9ImdnZ2dnIi8+IDxiPlx1N2IyY1x1NGU4Y1x1NmIyMTwvYj4="});
+define("b",[],function(y,d,r){r.exports=" <img src=\"/static/res/f1f9b8dfc1a39a0b236849b941393565.jpg\"  alt=\"ggggg\"/><img src=\"/static/res/f1f9b8dfc1a39a0b236849b941393565.jpg?a=2\"  alt=\"ggggg\"/> <b>第二次</b>"});
