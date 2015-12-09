57e5f9954cfc3d206d3e8c17ec338f7da6a7764c
cloudcome <cloudcome@163.com>
2015-12-09 12:04:52 +0800

    run test

--- example/dest/static/js/8b140bc18c86db9a4c0d327af3289a99.js
+++
@@ -1,2 +0,0 @@
-/*coolie@1.0.14*/
-coolie.config({base:"./app",async:"../async/",chunk:"../chunk/",debug:!1,cache:!0,version:{"../async/1.js":"ed7e8b81cd6519439bf78132ae597e49","../async/2.js":"e9c1e78519704801dc6b976f8a8d6d55","../async/3.js":"d312c07a3b408c31b398d9d64fa3d3a4"}}).use();
--- example/dest/static/js/async/2.e9c1e78519704801dc6b976f8a8d6d55.js
+++
@@ -1,2 +0,0 @@
-/*coolie@1.0.14*/
-define("0",[],function(e,n,t){document.getElementById("ret").innerHTML="page2"});
