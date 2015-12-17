431f63ec07917a68b90dd4e7b7701636feef8a21
cloudcome <cloudcome@163.com>
2015-12-17 10:06:28 +0800

    fix build html module coolie group

---
+++ example/dest/static/js/app/f41b0b39b854ed23ebc4d86f56c17972.js
@@ -0,0 +1,3 @@
+/*coolie@1.0.23*/
+define("0",["1"],function(t,e,i){"use strict";t("1")});
+define("1",[],function(y,d,r){r.exports="<link rel=\"stylesheet\" href=\"/static/css/fa306088889174f3ff8377580d809b02.css\"> "});
