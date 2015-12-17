62a2231ead8654a57231a832b11ae7dfdc271eed
cloudcome <cloudcome@163.com>
2015-12-17 14:35:55 +0800

    minify css

--- example/src/coolie.config.js
+++ example/src/coolie.config.js
@@ -20,7 +20,7 @@ module.exports = function (coolie) {
         },
         "html": {
             "src": [
-                "./html/style.html"
+                "./html/js.html"
             ],
             "minify": true
         },
