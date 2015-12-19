551ec0c5ab2862351fc998cffbdb4dccc87a8130
cloudcome <cloudcome@163.com>
2015-12-19 16:23:16 +0800

    more man

---
+++ example/dest/static/js/ffde1d74.js
@@ -0,0 +1,2 @@
+/*coolie@1.1.1*/
+coolie.config({base:"./app",async:"../async/",chunk:"../chunk/",debug:!1,cache:!0,builder:"1.1.1",version:{"../async/1.js":"ed7e8b81","../async/2.js":"e9c1e785","../async/3.js":"d312c07a"}}).use();
