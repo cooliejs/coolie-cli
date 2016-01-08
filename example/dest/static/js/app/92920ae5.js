21baa097493a6e7ebfdaa2a4d88ddeb0da764469
cloudcome <cloudcome@163.com>
2016-01-08 16:54:51 +0800

    support css|style module

---
+++ example/dest/static/js/app/92920ae5.js
@@ -0,0 +1,4 @@
+/*coolie@1.1.1*/
+define("0",["1","2"],function(s,e,i){"use strict";s("1");s("2")});
+define("1",[],function(y,d,r){r.exports="/static/res/6eb3c4ed.png"});
+define("2",[],function(y,d,r){coolie.importStyle("body{background:url(/static/res/f1f9b8df.jpg);content:\"\"}.div{background:url(/static/res/f1f9b8df.jpg)}")});
