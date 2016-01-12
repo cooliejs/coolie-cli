2fdf7a570b06f557b7b2611e190217cd95a7e73e
cloudcome <cloudcome@163.com>
2016-01-12 23:23:36 +0800

    middleware

---
+++ example/dest/static/js/58a9223ee30dfa05dd25e359dad398e0.js
@@ -0,0 +1,7 @@
+!function(){var a=1;alert(a);alert(a+2)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
+!function(){var a=2;alert(a)}();
