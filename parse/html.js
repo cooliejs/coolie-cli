c16323a19a11fe12a063d7703b921ed8c2b3e85e
cloudcome <cloudcome@163.com>
2016-01-12 15:34:29 +0800

    feature-posthtml

--- parse/html.js
+++ parse/html.js
@@ -1,5 +1,5 @@
 /**
- * 文件描述
+ * 解析 html
  * @author ydr.me
  * @create 2016-01-12 14:53
  */
@@ -8,6 +8,26 @@
 'use strict';
 
 var posthtml = require('posthtml');
+var allocation = require('ydr-utils').allocation;
+var dato = require('ydr-utils').dato;
 
-module.exports = posthtml;
+
+/**
+ * 解析 html
+ * @param code
+ * @returns {{String}}
+ */
+module.exports = function (code/*use list*/) {
+    var args = allocation.args(arguments);
+    var useList = args.slice(1);
+    var parser = posthtml();
+
+    dato.each(useList, function (index, user) {
+        parser = parser.use(user);
+    });
+
+    return parser.process(code, {
+        sync: true
+    }).html;
+};
 
