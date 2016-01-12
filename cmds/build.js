91023235d8816e5afcf2c42de4451ef3c49fc4c9
cloudcome <cloudcome@163.com>
2016-01-13 00:19:32 +0800

    middleware api

--- cmds/build.js
+++ cmds/build.js
@@ -11,6 +11,7 @@ var dato = require('ydr-utils').dato;
 var debug = require('ydr-utils').debug;
 var Middleware = require('ydr-utils').Middleware;
 var Emitter = require('ydr-utils').Emitter;
+var allocation = require('ydr-utils').allocation;
 
 var parseCoolieConfig = require('../parse/coolie.config.js');
 var buildAPP = require('../build/app.js');
@@ -79,8 +80,11 @@ emitter.on('exit', function (err) {
  * 构建主程序
  * @param options {Object} 配置
  * @param options.srcDirname {String} 构建根目录
+ * @param [end] {Function} 结束中间件模式
  */
-module.exports = function (options) {
+module.exports = function (options, end) {
+    var args = allocation.args(arguments);
+
     banner();
     options = dato.extend({}, defaults, options);
 
@@ -105,112 +109,119 @@ module.exports = function (options) {
     var destDirname = configs.destDirname;
     var coolieAPI = buildAPI(configs, middleware);
 
+    var next = function () {
+        // 2. 复制文件
+        console.log();
+        debug.primary(++stepIndex + '/' + stepLength, 'copy files');
+        var copiedList = buildCopy({
+            srcDirname: srcDirname,
+            destDirname: destDirname,
+            copy: configs.copy
+        });
+        if (!copiedList.length) {
+            debug.ignore('copy files', 'no files are copied');
+        }
 
-    // 2. 复制文件
-    console.log();
-    debug.primary(++stepIndex + '/' + stepLength, 'copy files');
-    var copiedList = buildCopy({
-        srcDirname: srcDirname,
-        destDirname: destDirname,
-        copy: configs.copy
-    });
-    if (!copiedList.length) {
-        debug.ignore('copy files', 'no files are copied');
+
+        // 3. 构建入口文件
+        console.log();
+        debug.primary(++stepIndex + '/' + stepLength, 'build main module');
+        var buildAPPResult = buildAPP({
+            glob: configs.js.main,
+            chunk: configs.js.chunk,
+            srcDirname: srcDirname,
+            destDirname: destDirname,
+            destCSSDirname: configs.destCSSDirname,
+            destResourceDirname: configs.destResourceDirname,
+            destHost: configs.destHost,
+            uglifyJSOptions: configs.uglifyJSOptions,
+            cleanCSSOptions: configs.cleanCSSOptions,
+            versionLength: configs.versionLength,
+            minifyResource: configs.minifyResource,
+            destCoolieConfigBaseDirname: configs.destCoolieConfigBaseDirname,
+            destCoolieConfigChunkDirname: configs.destCoolieConfigChunkDirname,
+            destCoolieConfigAsyncDirname: configs.destCoolieConfigAsyncDirname,
+            removeHTMLYUIComments: configs.removeHTMLYUIComments,
+            removeHTMLLineComments: configs.removeHTMLLineComments,
+            joinHTMLSpaces: configs.joinHTMLSpaces,
+            removeHTMLBreakLines: configs.removeHTMLBreakLines
+        });
+
+
+        // 3. 重写 coolie-config.js
+        console.log();
+        debug.primary(++stepIndex + '/' + stepLength, 'override coolie-config.js');
+        var destCoolieConfigJSPath = replaceCoolieConfig(configs.srcCoolieConfigJSPath, {
+            versionLength: configs.versionLength,
+            destCoolieConfigBaseDirname: configs.destCoolieConfigBaseDirname,
+            destCoolieConfigChunkDirname: configs.destCoolieConfigChunkDirname,
+            destCoolieConfigAsyncDirname: configs.destCoolieConfigAsyncDirname,
+            srcDirname: srcDirname,
+            destDirname: destDirname,
+            destJSDirname: configs.destJSDirname,
+            versionMap: dato.extend({}, buildAPPResult.chunkVersionMap, buildAPPResult.asyncVersionMap),
+            destHost: configs.destHost,
+            sign: true
+        });
+
+
+        // 4. 构建 html
+        console.log();
+        debug.primary(++stepIndex + '/' + stepLength, 'build html');
+        var buildHTMLResult = buildHTML({
+            middleware: middleware,
+            emitter: emitter,
+            glob: configs.html.src,
+            removeHTMLYUIComments: configs.removeHTMLYUIComments,
+            removeHTMLLineComments: configs.removeHTMLLineComments,
+            joinHTMLSpaces: configs.joinHTMLSpaces,
+            removeHTMLBreakLines: configs.removeHTMLBreakLines,
+            versionLength: configs.versionLength,
+            srcDirname: srcDirname,
+            destDirname: destDirname,
+            destJSDirname: configs.destJSDirname,
+            destCSSDirname: configs.destCSSDirname,
+            destResourceDirname: configs.destResourceDirname,
+            destHost: configs.destHost,
+            coolieConfigBase: configs.coolieConfigBase,
+            srcCoolieConfigJSPath: configs.srcCoolieConfigJSPath,
+            srcCoolieConfigBaseDirname: configs.srcCoolieConfigBaseDirname,
+            destCoolieConfigJSPath: destCoolieConfigJSPath,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyResource: true,
+            uglifyJSOptions: null,
+            cleanCSSOptions: configs.cleanCSSOptions,
+            replaceCSSResource: true,
+            mainVersionMap: buildAPPResult.mainVersionMap
+        });
+
+
+        // 5. 生成资源地图
+        console.log();
+        debug.primary(++stepIndex + '/' + stepLength, 'generate a resource relationship map');
+        buildMap({
+            srcDirname: srcDirname,
+            destDirname: destDirname,
+            configs: configs,
+            destCoolieConfigBaseDirname: configs.destCoolieConfigBaseDirname,
+            destCoolieConfigChunkDirname: configs.destCoolieConfigChunkDirname,
+            destCoolieConfigAsyncDirname: configs.destCoolieConfigAsyncDirname,
+            buildAPPResult: buildAPPResult,
+            buildHTMLResult: buildHTMLResult
+        });
+
+        var pastTime = Date.now() - beginTime;
+        console.log();
+        debug.primary('build success', 'past ' + pastTime + 'ms');
+        console.log();
+    };
+
+    if (args.length === 2) {
+        end(next);
+    } else {
+        next();
     }
-
-
-    // 3. 构建入口文件
-    console.log();
-    debug.primary(++stepIndex + '/' + stepLength, 'build main module');
-    var buildAPPResult = buildAPP({
-        glob: configs.js.main,
-        chunk: configs.js.chunk,
-        srcDirname: srcDirname,
-        destDirname: destDirname,
-        destCSSDirname: configs.destCSSDirname,
-        destResourceDirname: configs.destResourceDirname,
-        destHost: configs.destHost,
-        uglifyJSOptions: configs.uglifyJSOptions,
-        cleanCSSOptions: configs.cleanCSSOptions,
-        versionLength: configs.versionLength,
-        minifyResource: configs.minifyResource,
-        destCoolieConfigBaseDirname: configs.destCoolieConfigBaseDirname,
-        destCoolieConfigChunkDirname: configs.destCoolieConfigChunkDirname,
-        destCoolieConfigAsyncDirname: configs.destCoolieConfigAsyncDirname,
-        removeHTMLYUIComments: configs.removeHTMLYUIComments,
-        removeHTMLLineComments: configs.removeHTMLLineComments,
-        joinHTMLSpaces: configs.joinHTMLSpaces,
-        removeHTMLBreakLines: configs.removeHTMLBreakLines
-    });
-
-
-    // 3. 重写 coolie-config.js
-    console.log();
-    debug.primary(++stepIndex + '/' + stepLength, 'override coolie-config.js');
-    var destCoolieConfigJSPath = replaceCoolieConfig(configs.srcCoolieConfigJSPath, {
-        versionLength: configs.versionLength,
-        destCoolieConfigBaseDirname: configs.destCoolieConfigBaseDirname,
-        destCoolieConfigChunkDirname: configs.destCoolieConfigChunkDirname,
-        destCoolieConfigAsyncDirname: configs.destCoolieConfigAsyncDirname,
-        srcDirname: srcDirname,
-        destDirname: destDirname,
-        destJSDirname: configs.destJSDirname,
-        versionMap: dato.extend({}, buildAPPResult.chunkVersionMap, buildAPPResult.asyncVersionMap),
-        destHost: configs.destHost,
-        sign: true
-    });
-
-
-    // 4. 构建 html
-    console.log();
-    debug.primary(++stepIndex + '/' + stepLength, 'build html');
-    var buildHTMLResult = buildHTML({
-        middleware: middleware,
-        emitter: emitter,
-        glob: configs.html.src,
-        removeHTMLYUIComments: configs.removeHTMLYUIComments,
-        removeHTMLLineComments: configs.removeHTMLLineComments,
-        joinHTMLSpaces: configs.joinHTMLSpaces,
-        removeHTMLBreakLines: configs.removeHTMLBreakLines,
-        versionLength: configs.versionLength,
-        srcDirname: srcDirname,
-        destDirname: destDirname,
-        destJSDirname: configs.destJSDirname,
-        destCSSDirname: configs.destCSSDirname,
-        destResourceDirname: configs.destResourceDirname,
-        destHost: configs.destHost,
-        coolieConfigBase: configs.coolieConfigBase,
-        srcCoolieConfigJSPath: configs.srcCoolieConfigJSPath,
-        srcCoolieConfigBaseDirname: configs.srcCoolieConfigBaseDirname,
-        destCoolieConfigJSPath: destCoolieConfigJSPath,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyResource: true,
-        uglifyJSOptions: null,
-        cleanCSSOptions: configs.cleanCSSOptions,
-        replaceCSSResource: true,
-        mainVersionMap: buildAPPResult.mainVersionMap
-    });
-
-
-    // 5. 生成资源地图
-    console.log();
-    debug.primary(++stepIndex + '/' + stepLength, 'generate a resource relationship map');
-    buildMap({
-        srcDirname: srcDirname,
-        destDirname: destDirname,
-        configs: configs,
-        destCoolieConfigBaseDirname: configs.destCoolieConfigBaseDirname,
-        destCoolieConfigChunkDirname: configs.destCoolieConfigChunkDirname,
-        destCoolieConfigAsyncDirname: configs.destCoolieConfigAsyncDirname,
-        buildAPPResult: buildAPPResult,
-        buildHTMLResult: buildHTMLResult
-    });
-
-    var pastTime = Date.now() - beginTime;
-    console.log();
-    debug.primary('build success', 'past ' + pastTime + 'ms');
-    console.log();
 
     return coolieAPI;
 };
