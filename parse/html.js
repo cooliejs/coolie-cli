d1d48c55a73a13c24e5e0701cf24266d1ab309f9
cloudcome <cloudcome@163.com>
2016-01-12 14:56:09 +0800

    feature-posthtml

---
+++ parse/html.js
@@ -0,0 +1,13 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2016-01-12 14:53
+ */
+
+
+'use strict';
+
+var posthtml = require('posthtml');
+
+module.exports = posthtml;
+
---
+++ test/parse/html.js
@@ -0,0 +1,13 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2016-01-12 14:55
+ */
+
+
+'use strict';
+
+var fs = require('fs');
+
+
+
