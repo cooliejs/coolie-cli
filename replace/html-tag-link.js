ab4983bc664176668d7907ba5290cbd04f6a397e
cloudcome <cloudcome@163.com>
2016-01-12 16:58:08 +0800

    feature-posthtml

--- replace/html-tag-link.js
+++ replace/html-tag-link.js
@@ -74,7 +74,74 @@ module.exports = function (file, options) {
                 tag: 'link',
                 rel: 'stylesheet'
             }, function (node) {
-                return node;
+                if(!node.attrs || !node.attrs.href){
+                    return node;
+                }
+
+                if(node.attrs.hasOwnProperty(COOLIE_IGNORE)){
+                    node.attrs[COOLIE_IGNORE] = null;
+                    return node;
+                }
+
+                var href = node.attrs.href;
+
+                var srcPath = pathURI.toAbsoluteFile(href, file, options.srcDirname);
+                var destPath = minifyPathmap[srcPath];
+                var destURI = minifyCSSmap[srcPath];
+                resList = resourceMap[srcPath];
+
+                if (!destURI) {
+                    var srcCode = reader(srcPath, 'utf8');
+                    var destCode = srcCode;
+
+                    if (options.minifyCSS) {
+                        var minifyCSSRet = minifyCSS(srcPath, {
+                            code: srcCode,
+                            cleanCSSOptions: options.cleanCSSOptions,
+                            versionLength: options.versionLength,
+                            srcDirname: options.srcDirname,
+                            destDirname: options.destDirname,
+                            destHost: options.destHost,
+                            destResourceDirname: options.destResourceDirname,
+                            minifyResource: options.minifyResource,
+                            replaceCSSResource: true
+                        });
+                        destCode = minifyCSSRet.code;
+                        resList = minifyCSSRet.resList;
+                    }
+
+                    var destVersion = encryption.md5(destCode).slice(0, options.versionLength);
+
+                    destPath = path.join(options.destCSSDirname, destVersion + '.css');
+                    destURI = pathURI.toRootURL(destPath, options.destDirname);
+                    destURI = pathURI.joinURI(options.destHost, destURI);
+
+                    if (options.signCSS) {
+                        destCode = sign('css') + '\n' + destCode;
+                    }
+
+                    try {
+                        fse.outputFileSync(destPath, destCode, 'utf8');
+                        minifyPathmap[srcPath] = destPath;
+                        minifyCSSmap[srcPath] = destURI;
+                        resourceMap[srcPath] = resList;
+                        debug.success('√', pathURI.toRootURL(srcPath, options.srcDirname));
+                    } catch (err) {
+                        debug.error('write file', path.toSystem(destPath));
+                        debug.error('write file', err.message);
+                        return process.exit(1);
+                    }
+                }
+
+                cssList.push({
+                    destPath: destPath,
+                    dependencies: [{
+                        srcPath: srcPath,
+                        resList: resList
+                    }]
+                });
+
+                node.attrs.href = destURI;
             });
         })
         .get();
