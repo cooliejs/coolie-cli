5bc5d8eeaea2d1e2381d454662623828c54956e4
cloudcome <cloudcome@163.com>
2016-01-12 15:03:36 +0800

    feature-posthtml

--- test/parse/html.js
+++ test/parse/html.js
@@ -7,7 +7,25 @@
 
 'use strict';
 
+
+var path = require('ydr-utils').path;
 var fs = require('fs');
+
+var parseHTML = require('../../parse/html.js');
+
+
+var file = path.join(__dirname, 'test.html');
+var code = fs.readFileSync(file, 'utf8');
+
+describe('parse/html.js', function () {
+    it('1', function () {
+        var ret = parseHTML().process(code);
+
+        console.log(ret);
+    });
+});
+
+
 
 
 
