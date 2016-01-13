facceb1c34dd839415992542f3fd985d0fc11300
cloudcome <cloudcome@163.com>
2016-01-13 15:55:34 +0800

    template

--- cmds/create-express.js
+++ cmds/create-express.js
@@ -9,6 +9,9 @@
 
 var npm = require('ydr-utils').npm;
 var path = require('ydr-utils').path;
+var allocation = require('ydr-utils').allocation;
+var dato = require('ydr-utils').dato;
+var debug = require('ydr-utils').debug;
 var howdo = require('howdo');
 
 var root = path.join(__dirname, '../template/express/');
@@ -29,4 +32,52 @@ var devDependencies = [
     'mocha',
     'supervisor'
 ];
+
+
+var getModulesVersion = function (modules, callback) {
+    howdo
+        .each(modules, function (index, dep, done) {
+            npm.getLatestVersion(dep, function (err, version) {
+                done(err, version);
+            });
+        })
+        .together()
+        .try(function () {
+            var args = allocation.args(arguments);
+            var deps = {};
+
+            dato.each(args, function (index, version) {
+                var name = modules[index];
+                deps[name] = version;
+            });
+
+            callback(null, deps);
+        })
+        .catch(callback);
+};
+
+
+/**
+ * 生成 express 模板
+ * @param options {Object} 配置
+ * @param options.destDirname {String} 目标目录
+ */
+module.exports = function (options) {
+    howdo
+    // dependencies
+        .task(function (done) {
+            getModulesVersion(dependencies, done);
+        })
+        // devDependencies
+        .task(function (done) {
+            getModulesVersion(devDependencies, done);
+        })
+        .together()
+        .try(function (dependencies, devDependencies) {
+
+        })
+        .catch(function (err) {
+            debug.error('create error', err.message);
+        });
+};
 
