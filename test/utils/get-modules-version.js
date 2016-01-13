f3faa4a47321e5d67034b8fef0ecdf2192b8c662
cloudcome <cloudcome@163.com>
2016-01-13 16:37:22 +0800

    template

---
+++ test/utils/get-modules-version.js
@@ -0,0 +1,38 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2016-01-13 16:34
+ */
+
+
+'use strict';
+
+var assert = require('assert');
+
+var getModulesVersion = require('../../utils/get-modules-version.js');
+
+describe('get-modules-version', function () {
+    it('e', function (done) {
+        var modules = [
+            'npm',
+            'coolie',
+            'express'
+        ];
+
+        getModulesVersion(modules, function (err, versions) {
+            if (err) {
+                console.log(err.message);
+            }
+
+            if (versions) {
+                console.log(versions);
+                assert.equal(!!versions.npm, true);
+                assert.equal(!!versions.coolie, true);
+                assert.equal(!!versions.express, true);
+            }
+
+            done();
+        });
+    });
+});
+
