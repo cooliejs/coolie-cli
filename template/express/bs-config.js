8b95c5dd6bed0dd78fef6c835915726edb60b763
cloudcome <cloudcome@163.com>
2016-01-13 20:03:55 +0800

    fix template

---
+++ template/express/bs-config.js
@@ -0,0 +1,105 @@
+// 当前 express 的 url
+var EXPRESS_URL = 'http://localhost:10000';
+
+//  Browser-sync 启动的端口，不能与 express 端口一致
+var BROWSER_SYNC_PORT = 10001;
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "ui": {
+        "port": 3001,
+        "weinre": {
+            "port": 8080
+        }
+    },
+    "files": [
+        './webroot-dev/static/**',
+        './webroot-dev/.views/**',
+        './webserver/**'
+    ],
+    "watchOptions": {},
+    "server": false,
+    "proxy": EXPRESS_URL,
+    "port": BROWSER_SYNC_PORT,
+    "middleware": false,
+    "serveStatic": [],
+    "ghostMode": {
+        "clicks": true,
+        "scroll": true,
+        "forms": {
+            "submit": true,
+            "inputs": true,
+            "toggles": true
+        }
+    },
+    "logLevel": "info",
+    "logPrefix": "BS",
+    "logConnections": false,
+    "logFileChanges": true,
+    "logSnippet": true,
+    "rewriteRules": false,
+    "open": "external",
+    "browser": "default",
+    "xip": false,
+    "hostnameSuffix": false,
+    "reloadOnRestart": false,
+    "notify": true,
+    "scrollProportionally": true,
+    "scrollThrottle": 0,
+    "scrollRestoreTechnique": "window.name",
+    "scrollElements": [],
+    "scrollElementMapping": [],
+    "reloadDelay": 0,
+    "reloadDebounce": 0,
+    "plugins": [],
+    "injectChanges": true,
+    "startPath": null,
+    "minify": true,
+    "host": null,
+    "codeSync": true,
+    "timestamps": true,
+    "clientEvents": [
+        "scroll",
+        "scroll:element",
+        "input:text",
+        "input:toggles",
+        "form:submit",
+        "form:reset",
+        "click"
+    ],
+    "socket": {
+        "socketIoOptions": {
+            "log": false
+        },
+        "path": "/browser-sync/socket.io",
+        "clientPath": "/browser-sync",
+        "namespace": "/browser-sync",
+        "clients": {
+            "heartbeatTimeout": 5000
+        }
+    },
+    "tagNames": {
+        "less": "link",
+        "scss": "link",
+        "css": "link",
+        "jpg": "img",
+        "jpeg": "img",
+        "png": "img",
+        "svg": "img",
+        "gif": "img",
+        "js": "script"
+    }
+};
