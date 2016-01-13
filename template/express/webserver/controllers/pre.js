9a1945cf4faa466a5ee363039a32ef2199a04ff9
cloudcome <cloudcome@163.com>
2016-01-13 15:13:00 +0800

    template

---
+++ template/express/webserver/controllers/pre.js
@@ -0,0 +1,14 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2016-01-13 15:12
+ */
+
+
+'use strict';
+
+module.exports = function (app) {
+
+};
+
+
---
+++ template/express/webserver/middlewares/parser.js
@@ -0,0 +1,30 @@
+/**
+ * 解析类中间件
+ * @author ydr.me
+ * @create 2016-01-13 15:11
+ */
+
+
+'use strict';
+
+
+// 解析 ua
+exports.parseUA = function (req, res, next) {
+    var ua = req.headers['user-agent'];
+    var isMobile = /mobile|iphone|ipad|ipod|andorid/i.test(ua);
+    var isWeixin = /MicroMessenger\b/i.test(ua);
+    var isDangkr = /dangkr\b/.test(ua);
+    var isIOS = /iphone|ipod|ipad/i.test(ua);
+    var isAOS = /android/i.test(ua);
+
+    res.locals.$ua = req.session.$ua = {
+        isMobile: isMobile,
+        isWeixin: isWeixin,
+        isDangkr: isDangkr,
+        isIOS: isIOS,
+        isAOS: isAOS
+    };
+    next();
+};
+
+
