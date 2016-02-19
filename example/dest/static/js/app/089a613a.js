3f8df9c1d3581736e636e4598616c6ce00f66171
cloudcome <cloudcome@163.com>
2016-02-19 11:08:31 +0800

    copy safe

---
+++ example/dest/static/js/app/089a613a.js
@@ -0,0 +1,4 @@
+/*coolie build*/
+define("0",["1","2"],function(t,f,i){"use strict";t("1");t("2")});
+define("1",[],function(y,d,r){r.exports="<link rel=\"stylesheet\" href=\"/static/css/915d7e0b.css\"><style>link{background:url(/static/res/f1f9b8df.jpg)}</style>"});
+define("2",[],function(y,d,r){r.exports="/static/res/8bd19d35.woff"});
