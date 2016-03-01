2fca833de582fd0a6253cbf9efdc5f9e45c50c16
cloudcome <cloudcome@163.com>
2016-03-01 11:40:05 +0800

    deep async

--- example/src/static/js/app/async.js
+++ example/src/static/js/app/async.js
@@ -1,29 +1,31 @@
 define(function (require, exports, module) {
     'use strict';
 
-    require('../libs1/all.js');
-    //require('../libs2/some.css', 'css');
-    //require('../libs2/some.html', 'html|url');
-    //require('../libs2/some.jpg', 'image|url');
-    //require('../libs2/some.json', 'json');
-    //require('../libs2/some.txt', 'text');
+    require.async('../libs2/all-1-2.js');
 
-    var listenHash = function () {
-        switch (location.hash) {
-            case '#page1':
-                require.async('../pages/page1.js');
-                break;
-
-            case '#page2':
-                require.async('../pages/page2.js');
-                break;
-
-            default :
-                require.async('../pages/404.js');
-                break;
-        }
-    };
-
-    listenHash();
-    window.addEventListener('hashchange', listenHash);
+    //require('../libs1/all.js');
+    ////require('../libs2/some.css', 'css');
+    ////require('../libs2/some.html', 'html|url');
+    ////require('../libs2/some.jpg', 'image|url');
+    ////require('../libs2/some.json', 'json');
+    ////require('../libs2/some.txt', 'text');
+    //
+    //var listenHash = function () {
+    //    switch (location.hash) {
+    //        case '#page1':
+    //            require.async('../pages/page1.js');
+    //            break;
+    //
+    //        case '#page2':
+    //            require.async('../pages/page2.js');
+    //            break;
+    //
+    //        default :
+    //            require.async('../pages/404.js');
+    //            break;
+    //    }
+    //};
+    //
+    //listenHash();
+    //window.addEventListener('hashchange', listenHash);
 });
