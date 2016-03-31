f1d318fbbcc5362fceaa6bf5dfc702e6945e9293
cloudcome <cloudcome@163.com>
2016-03-31 11:56:17 +0800

    host function

---
+++ example/dest/static/js/dbb4b251174e6bcef007d9f29843589c.js
@@ -0,0 +1,1 @@
+coolie.config({base:"./app",async:"../async/",chunk:"../chunk/",debug:!1,cache:!0,builder:"1.9.0",version:{"../index3-1.js":"cfcd208495d565ef66e7dff9f98764da","user/index.js":"cfcd208495d565ef66e7dff9f98764da"}}).use();
