e676670d88bfed4733d463971ddae11063b991f5
cloudcome <cloudcome@163.com>
2016-04-12 10:47:17 +0800

    data

--- minify/js.js
+++ minify/js.js
@@ -17,51 +17,67 @@ var compressorOptions = {
     // 连续单语句，逗号分开
     // 如： alert(1);alert(2); => alert(1),alert(2)
     sequences: false,
+
     // 重写属性
     // 如：foo['bar'] => foo.bar
     properties: false,
+
     // 删除无意义代码
     dead_code: false,
+
     // 移除`debugger;`
     drop_debugger: true,
+
     // 使用不安全的压缩
     unsafe: false,
+
     // 不安全压缩
     unsafe_comps: false,
+
     // 压缩if表达式
     // if(abc) { dosth. } => abc&&dosth.
     conditionals: true,
+
     // 压缩比较表达式，unsafe === true
     // !(a <= b) => a > b
     // a = !b && !c && !d && !e => a=!(b||c||d||e)
     comparisons: false,
+
     // 压缩常数表达式，移除无用的常量判断
     // if(DEBUG){return 1}else{return 2} => return 2
     // if(!1 == false){..} => EMPTY
     evaluate: true,
+
     // 压缩布尔值
     booleans: true,
+
     // 压缩循环
     loops: true,
+
     // 移除未使用变量
     // function(){ var a = 1; return 1;} => function(){return 1;}
     unused: true,
+
     // 函数声明提前
     hoist_funs: true,
+
     // 变量声明提前
     hoist_vars: false,
+
     // 压缩 if return if continue
     if_return: true,
+
     // 合并连续变量省略
     join_vars: true,
+
     // 小范围连续变量压缩
     cascade: true,
+
     // 显示警告语句
     warnings: false,
-    // 全局变量，会在构建之后，删除
-    global_defs: {
-        DEBUG: false
-    }
+
+    // 全局常量，会在构建之后，删除
+    global_defs: {}
 };
 
 
