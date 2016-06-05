f04047fae1196a3c28b896a56262e2a507c14d11
cloudcome <cloudcome@163.com>
2016-06-05 18:37:48 +0800

    重构：banner 展示

--- src/cmds/banner.js
+++ src/cmds/banner.js
@@ -1,5 +1,5 @@
 /**
- * 文件描述
+ * banner
  * @author ydr.me
  * @create 2015-10-31 20:35
  */
@@ -8,18 +8,114 @@
 
 'use strict';
 
+var debug = require('blear.node.debug');
+var console = require('blear.node.console');
+
+
 var pkg = require('../../package.json');
+
+
+var blank = function (length) {
+    return new Array(length).join(' ');
+};
 
 module.exports = function () {
     console.log();
-    console.table([
-        ['coolie@' + pkg.version],
-        [pkg.description],
-        ['官网：'+pkg.homepage]
-    ], {
-        colors: ['green'],
-        padding: 1
-    });
-
+    console.log(
+        console.colors.original(blank(11)),
+        console.colors.original(blank(11)),
+        console.colors.original(blank(12)),
+        console.colors.green('oooo'),
+        console.colors.original(blank(3)),
+        console.colors.magenta('o8o'),
+        console.colors.original(blank(12))
+    );
+    console.log(
+        console.colors.original(blank(34)),
+        console.colors.green('`888'),
+        console.colors.original(blank(3)),
+        console.colors.magenta('`"\'')
+    );
+    console.log(
+        console.colors.original(blank(2)),
+        console.colors.red('.ooooo.'),
+        console.colors.original(blank(3)),
+        console.colors.yellow('.ooooo.'),
+        console.colors.original(blank(3)),
+        console.colors.cyan('.ooooo.'),
+        console.colors.original(blank(3)),
+        console.colors.green('888'),
+        console.colors.original(blank(2)),
+        console.colors.magenta('oooo'),
+        console.colors.original(blank(3)),
+        console.colors.blue('.ooooo.')
+    );
+    console.log(
+        console.colors.original(blank(1)),
+        console.colors.red('d88\' `"Y8'),
+        console.colors.original(blank(1)),
+        console.colors.yellow('d88\' `88b'),
+        console.colors.original(blank(1)),
+        console.colors.cyan('d88\' `88b'),
+        console.colors.original(blank(2)),
+        console.colors.green('888'),
+        console.colors.original(blank(2)),
+        console.colors.magenta('`888'),
+        console.colors.original(blank(2)),
+        console.colors.blue('d88\' `88b')
+    );
+    console.log(
+        console.colors.original(blank(1)),
+        console.colors.red('888'),
+        console.colors.original(blank(7)),
+        console.colors.yellow('888   888'),
+        console.colors.original(blank(1)),
+        console.colors.cyan('888   888'),
+        console.colors.original(blank(2)),
+        console.colors.green('888'),
+        console.colors.original(blank(3)),
+        console.colors.magenta('888'),
+        console.colors.original(blank(2)),
+        console.colors.blue('888ooo888')
+    );
+    console.log(
+        console.colors.original(blank(1)),
+        console.colors.red('888   .o8'),
+        console.colors.original(blank(1)),
+        console.colors.yellow('888   888'),
+        console.colors.original(blank(1)),
+        console.colors.cyan('888   888'),
+        console.colors.original(blank(2)),
+        console.colors.green('888'),
+        console.colors.original(blank(3)),
+        console.colors.magenta('888'),
+        console.colors.original(blank(2)),
+        console.colors.blue('888    .o')
+    );
+    console.log(
+        console.colors.original(blank(1)),
+        console.colors.red('`Y8bod8P\''),
+        console.colors.original(blank(1)),
+        console.colors.yellow('`Y8bod8P\''),
+        console.colors.original(blank(1)),
+        console.colors.cyan('`Y8bod8P\''),
+        console.colors.original(blank(1)),
+        console.colors.green('o888o'),
+        console.colors.original(blank(1)),
+        console.colors.magenta('o888o'),
+        console.colors.original(blank(1)),
+        console.colors.blue('`Y8bod8P\''),
+        console.colors.original(blank(2)),
+        console.colors.bgRed(' ' + pkg.version + ' ')
+    );
+    console.log();
+    console.log(
+        console.colors.original(blank(1)),
+        console.colors.grey(new Array(20).join('━')),
+        console.colors.original(blank(1)),
+        console.colors.original(pkg.description),
+        console.colors.grey(new Array(20).join('━')),
+        console.colors.original(blank(1))
+    );
     console.log();
 };
--- src/cmds/help.js
+++ src/cmds/help.js
@@ -8,43 +8,40 @@
 'use strict';
 
 var fs = require('fs');
-var debug = require('ydr-utils').debug;
-var path = require('ydr-utils').path;
+var debug = require('blear.node.debug');
+var console = require('blear.node.console');
+
 
 var banner = require('./banner.js');
-
-var bannerPath = path.join(__dirname, '../data/banner.txt');
-var bannerText = fs.readFileSync(bannerPath, 'utf8');
-
 
 module.exports = function () {
     var options = {
         nameAlign: 'left'
     };
-
-    console.log(console.styles.pretty(bannerText, 'green'));
+    
     banner();
     console.log();
-    console.log('1. 命令');
-    debug.success('   build', '前端工程化构建', options);
-    debug.success('   book', '打开 coolie 官方指南', options);
-    // debug.success('   install <module>', 'install a coolie module', options);
-    debug.success('   init', '初始化配置文件', options);
-    debug.success('   create', '创建一个 coolie 样板工程', options);
-    debug.success('   demo <demoId>', '下载 coolie 官方示例', options);
-    debug.success('   help', '打印帮助信息', options);
-    debug.success('   version', '打印版本信息', options);
+    console.log('1. Commands');
+    debug.normal('   build', '前端工程化构建', options);
+    debug.normal('   book', '打开 coolie 官方指南', options);
+    // debug.normal('   install <module>', 'install a coolie module', options);
+    debug.normal('   init', '初始化配置文件', options);
+    debug.normal('   create', '创建一个 coolie 工程脚手架', options);
+    debug.normal('   demo <demoId>', '阅读 coolie 官方示例', options);
+    debug.normal('   help', '打印帮助信息', options);
+    debug.normal('   version', '打印版本信息', options);
     console.log();
 
-    console.log('2. 参数');
-    debug.success('   -h --help', '打印命令的帮助信息', options);
-    debug.success('   -d --dirname', '指定目标目录，默认为当前工作目录', options);
-    debug.success('   -j --coolie.js', '初始化模块加载器配置文件，生成文件名为`coolie-config.js', options);
-    debug.success('   -c --coolie-cli', '初始化前端工程化构建配置文件，生成文件名为`coolie.config.js', options);
-    debug.success('   -e --express', '选择 express 全栈工程样板', options);
-    debug.success('   -s --static', '选择静态工程样板', options);
-    debug.success('   -r --redis', '是否在 express 样板中使用 redis', options);
-    debug.success('   -m --mongoose', '是否在 express 样板中使用 mongoose', options);
+    console.log('2. Options');
+    debug.normal('   -h --help', '打印命令的帮助信息', options);
+    debug.normal('   -d --dirname', '指定目标目录，默认为当前工作目录', options);
+    debug.normal('   -j --coolie.js', '初始化模块加载器配置文件，生成文件名为`coolie-config.js', options);
+    debug.normal('   -c --coolie-cli', '初始化前端工程化构建配置文件，生成文件名为`coolie.config.js', options);
+    debug.normal('   -C --configFile', '指定构建时使用的配置文件，默认为`coolie.config.js`', options);
+    debug.normal('   -e --express', '选择 express 全栈工程脚手架', options);
+    debug.normal('   -s --static', '选择静态工程脚手架', options);
+    debug.normal('   -r --redis', '是否在 express 工程脚手架中使用 redis', options);
+    debug.normal('   -m --mongoose', '是否在 express 工程脚手架中使用 mongoose', options);
     console.log();
 };
 
