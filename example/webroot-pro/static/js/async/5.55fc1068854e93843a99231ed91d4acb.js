82bfa81b934750cc79a644765f020c3f4b4652b0
cloudcome <cloudcome@163.com>
2016-06-22 15:34:31 +0800

    修正 $ 符号 replace

---
+++ example/webroot-pro/static/js/async/5.55fc1068854e93843a99231ed91d4acb.js
@@ -0,0 +1,12 @@
+/*coolie built*/
+
+/* @ref /static/js/async/[coolie-virtual-file]-2016062215312600.js */
+define("0", ["1"], function (require, exports, module) {module.exports = require("1");
+
+});
+
+
+/* @ref /static/js/async/page1.js */
+define("1", [], function (require, exports, module) {module.exports = 'page1'
+
+});
