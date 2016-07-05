5317b41b53ce72268250fb5dcd73a44055ada320
cloudcome <cloudcome@163.com>
2016-07-05 15:34:07 +0800

    rel=bookmark

---
+++ example/dest/static/js/app/c871d151.js
@@ -0,0 +1,4 @@
+/*coolie built*/
+define("0",["5","6"],function(t,f,i){"use strict";t("5");t("6")});
+define("5",[],function(y,d,r){r.exports="<link rel=\"stylesheet\" href=\"/static/css/fb62d10a.css\"><style>link{background:url(/static/res/f1f9b8df.jpg)}</style>"});
+define("6",[],function(y,d,r){r.exports="/static/res/8bd19d35.woff"});
---
+++ example/dest/static/js/async/f.082aad6e.js
@@ -0,0 +1,3 @@
+/*coolie built*/
+define("0",["8"],function(n){return n("8")});
+define("8",[],function(s,l,e){"use strict";s.async("e");e.exports="/libs1/all.js"});
