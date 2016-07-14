d7038c266f3623952d289cfbc7f16173f7784175
cloudcome <cloudcome@163.com>
2016-07-14 17:24:12 +0800

    test

---
+++ test/replace/dest/static/js/beea7831758595a3b2810de8e6dc4be1.js
@@ -0,0 +1,1 @@
+coolie.config({debug:!1,mode:"AMD",asyncModulesMap:{},chunkModulesMap:{file1:"cfcd208495d565ef66e7dff9f98764da",file2:"cfcd208495d565ef66e7dff9f98764da"},built:"coolie@2.0.0-beta4",mainModulesDir:"undefined",asyncModulesDir:"../async/",chunkModulesDir:"../chunk/",global:"undefined"}).use();
