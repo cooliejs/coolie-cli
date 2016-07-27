66ddd56f484c0ad8d0218f5f356c3aa21cfbb500
cloudcome <cloudcome@163.com>
2016-07-27 18:08:18 +0800

    ~ CI 测试

--- test/test.index.js
+++ test/test.index.js
@@ -7,22 +7,13 @@
 
 'use strict';
 
-var assert = require('assert');
-var path = require('ydr-utils').path;
+var url = require('blear.utils.url');
 
-var index = require('../index.js');
 
-var srcDirname= path.join(__dirname, '../example/src');
 
 describe('index.js', function () {
     it('e', function () {
-        //var coolie = index({
-        //    srcDirname: srcDirname
-        //}, function (next) {
-        //    next();
-        //});
-        //
-        //assert.equal(!!coolie.matchHTML, true);
+        console.log(url.join('/', 'static/res/abc.png'));
     });
 });
 
