dcfe54bcf26778876bc244d34d235d9577bfc040
cloudcome <cloudcome@163.com>
2016-07-28 10:07:33 +0800

    ~ CI 测试

--- test/test.index.js
+++ test/test.index.js
@@ -8,12 +8,14 @@
 'use strict';
 
 var url = require('blear.utils.url');
+var path = require('blear.utils.path');
 
 
 
 describe('index.js', function () {
     it('e', function () {
         console.log(url.join('/', 'static/res/abc.png'));
+        console.log(path.join('/', 'static/res/abc.png'));
     });
 });
 
