defa39148ea2a12f692e36c9ed80ef3ac60b025a
cloudcome <cloudcome@163.com>
2016-12-02 15:55:41 +0800

    ~ 去除打印日志

--- example/webroot-pro/static/js/async/5.07a1c9e2049b339c758c871bf8eba32b.js
+++ example/webroot-pro/static/js/async/5.07a1c9e2049b339c758c871bf8eba32b.js
@@ -1,6 +1,6 @@
 /*coolie built*/
 
-/* @ref //abc.com/1/2/3/static/js/async/[coolie-virtual-file]-201612021532200W.js */
+/* @ref //abc.com/1/2/3/static/js/async/[coolie-virtual-file]-201612021554440W.js */
 define("0", ["1"], function (require, exports, module) {module.exports = require("1");
 
 });
--- example/webroot-pro/static/js/cbd65d5e416a54a66e01a83227845956.js
+++ example/webroot-pro/static/js/cbd65d5e416a54a66e01a83227845956.js
@@ -2,9 +2,9 @@
 coolie.config({
     debug: false,
     mode: "AMD",
-    asyncModulesMap: {"5":"da5c077803387aaca90d5c59c51bf5e5"},
-    chunkModulesMap: {"0":"e0dde24a65ef134f1e0178cd3fbcbc32"},
-    built: "coolie@2.0.7",
+    asyncModulesMap: {"5":"07a1c9e2049b339c758c871bf8eba32b"},
+    chunkModulesMap: {"0":"7bf49ba13df5607d9bbc18cb3d1bf0fe"},
+    built: "coolie@2.0.9",
     mainModulesDir:"/1/2/3/static/js/main/",
     asyncModulesDir:"../async/",
     chunkModulesDir:"../chunk/",
