d2e6e80e02804b912bbc1ab12ce5ac43dc8862f7
cloudcome <cloudcome@163.com>
2018-09-11 10:23:05 +0800

    ~ 分离 resolve-module util

--- src/parse/require.js
+++ src/parse/require.js
@@ -7,23 +7,13 @@
 
 'use strict';
 
-var U2 = require("uglify-js");
-var path = require('blear.node.path');
 var debug = require('blear.node.debug');
 var console = require('blear.node.console');
 
-
 var globalId = require('../utils/global-id.js');
-var pathURI = require('../utils/path-uri.js');
-var requirePipeline = require('../parse/require-pipeline.js');
 var parseRequireNodeList = require('./require-node-list.js');
-var bookURL = require('../utils/book-url');
+var resolveModule = require('../utils/resolve-module');
 
-var PACKAGE_JSON = 'package.json';
-var NODE_MODULES = 'node_modules';
-var reAbsolute = /^\//;
-var reRelative = /^\.{1,2}\//;
-var pathRE = /\//;
 
 
 /**
@@ -54,68 +44,22 @@ module.exports = function (file, options) {
         process.exit(1);
     }
 
-    var nodeModuleMainPath = coolieConfigs.nodeModuleMainPath;
     nodeList.forEach(function (node) {
         var async = node.expression.property === 'async';
         var arg0 = node.args[0];
         var arg1 = node.args[1];
-        var name = arg0.value;
-        var fullName = name;
-        var pipeLine = requirePipeline(file, name, async ? '' : (arg1 && arg1.value || ''));
-        var inType = pipeLine[0];
-        var outType = pipeLine[1];
-        var requireFile;
-        var nodeModule = false;
 
-        // 相对于根目录
-        if (reAbsolute.test(name)) {
-            requireFile = path.join(options.srcDirname, name);
-        }
-        // 相对于当前目录
-        else if (reRelative.test(name)) {
-            requireFile = path.join(path.dirname(file), name);
-        }
-        // 相对于 node_modules 目录
-        else {
-            nodeModule = true;
-            var nodeModuleDirname = name;
-            var nodeModulePath = nodeModuleMainPath;
-
-            // require('node-module/path/to/module.js');
-            if (pathRE.test(name)) {
-                var com = name.split(pathRE);
-                nodeModuleDirname = com.shift();
-                nodeModulePath = com.join('/');
-            }
-
-            var fromDirname = path.join(options.srcCoolieConfigNodeModulesDirname, nodeModuleDirname);
-
-            if (nodeModulePath) {
-                requireFile = path.join(fromDirname, nodeModulePath);
-            } else {
-                var reqPkg = {};
-                var pkgJSONFile = path.join(fromDirname, PACKAGE_JSON);
-                try {
-                    // 依赖模块的描述文件
-                    reqPkg = require(pkgJSONFile);
-                } catch (err) {
-                    console.log();
-                    debug.error('node_module', file + '\n依赖的`' + name + '` 模块描述文件不存在或语法错误');
-                    debug.error('package.json', pkgJSONFile);
-                    debug.warn('warning', '模块路径指南 <' + bookURL('/introduction/module-path/') + '>');
-                    return process.exit(1);
-                }
-
-                requireFile = path.join(fromDirname, reqPkg.main || 'index.js');
-            }
-        }
-
-        var extname = path.extname(requireFile);
-
-        if (inType === 'js' && extname !== '.js') {
-            requireFile += '.js';
-            fullName += '.js';
-        }
+        var res = resolveModule(arg0.value, arg1 && arg1.value, {
+            file: file,
+            srcDirname: options.srcDirname,
+            srcNodeModulesDirname: options.srcCoolieConfigNodeModulesDirname
+        });
+        var requireFile = res.id;
+        var name = res.name;
+        var fullName = res.fullName;
+        var inType = res.inType;
+        var outType = res.outType;
+        var nodeModule = res.nodeModule;
 
         if (options.middleware) {
             var meta = options.middleware.exec({
@@ -131,12 +75,6 @@ module.exports = function (file, options) {
             });
 
             requireFile = meta.file;
-            // name = meta.name;
-            // fullname = meta.fullname;
-            // inType = meta.inType;
-            // outType = meta.outType;
-            // async = meta.async;
-            // nodeModule = meta.nodeModule;
         }
 
         requireList.push({
---
+++ src/utils/resolve-module.js
@@ -0,0 +1,109 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2018-09-11 09:50
+ * @update 2018-09-11 09:50
+ */
+
+
+'use strict';
+
+var path = require('blear.node.path');
+var debug = require('blear.node.debug');
+var console = require('blear.node.console');
+
+var requirePipeline = require('../parse/require-pipeline.js');
+var bookURL = require('./book-url');
+
+
+var reAbsolute = /^\//;
+var reRelative = /^\.{1,2}\//;
+var pathRE = /\//;
+var PACKAGE_JSON = 'package.json';
+
+
+/**
+ * 解析模块路径
+ * @param name
+ * @param pipeline
+ * @param options
+ * @param options.file
+ * @param options.srcDirname
+ * @param [options.srcNodeModulesDirname]
+ * @returns {*}
+ */
+module.exports = function (name, pipeline, options) {
+    var file = options.file;
+    var fullName = name;
+    var requireFile;
+    var nodeModule = false;
+    var pipeLine = requirePipeline(file, name, pipeline);
+    var inType = pipeLine[0];
+    var outType = pipeLine[1];
+
+    // 相对于根目录
+    if (reAbsolute.test(name)) {
+        requireFile = path.join(options.srcDirname, name);
+    }
+    // 相对于当前目录
+    else if (reRelative.test(name)) {
+        requireFile = path.join(path.dirname(file), name);
+    }
+    // 相对于 node_modules 目录
+    else {
+        nodeModule = true;
+        var nodeModuleDirname = name;
+        var nodeModulePath;
+
+        // require('node-module/path/to/module.js');
+        if (pathRE.test(name)) {
+            var com = name.split(pathRE);
+            nodeModuleDirname = com.shift();
+            nodeModulePath = com.join('/');
+        }
+
+        var srcNodeModulesDirname = options.srcNodeModulesDirname ?
+            options.srcNodeModulesDirname :
+            path.join(options.srcDirname, 'node_modules');
+        var fromDirname = path.join(srcNodeModulesDirname, nodeModuleDirname);
+
+        if (nodeModulePath) {
+            requireFile = path.join(fromDirname, nodeModulePath);
+        } else {
+            var reqPkg = {};
+            var pkgJSONFile = path.join(fromDirname, PACKAGE_JSON);
+            try {
+                // 依赖模块的描述文件
+                reqPkg = require(pkgJSONFile);
+            } catch (err) {
+                console.log();
+                debug.error('node_module', file + '\n依赖的`' + name + '` 模块描述文件不存在或语法错误');
+                debug.error('package.json', pkgJSONFile);
+                debug.warn('warning', '模块路径指南 <' + bookURL('/introduction/module-path/') + '>');
+                return process.exit(1);
+            }
+
+            requireFile = path.join(fromDirname, reqPkg.main || 'index.js');
+        }
+    }
+
+    var extname = path.extname(requireFile);
+
+    if (inType === 'js' && extname !== '.js') {
+        requireFile += '.js';
+        fullName += '.js';
+    }
+
+    return {
+        id: requireFile,
+        inType: inType,
+        outType: outType,
+        name: name,
+        pipeline: pipeline,
+        fullName: fullName,
+        nodeModule: nodeModule
+    };
+};
+
+
+
